4228cf8706973fe508f47eedbb40b92711e4adeb
ondro <777ondro@gmail.com>
2018-08-17 11:53:11 +0200

    Export to Word

---
+++ EXPIMP/ExportToWordDocument.cs
@@ -0,0 +1,87 @@
+﻿using Xceed.Words.NET;
+
+namespace EXPIMP
+{
+    public static class ExportToWordDocument
+    {
+        public static void ExportSomethingToWordDoc(string filename)
+        {
+            // Create a new document.
+            using (DocX document = DocX.Create(filename))
+            {
+                // The path to a template document,
+                //string templatePath = Server.MapPath("~/res/") + @"Template.docx";
+                // Apply a template to the document based on a path.
+                //document.ApplyTemplate(templatePath);
+
+                document.InsertParagraph("test text");
+                CreateChapterWithBuletedList(document);
+
+                CreateTOC(document);
+                
+                // Save this document to disk.
+                document.Save();
+            }
+        }
+
+        private static void CreateTOC(DocX document)
+        {
+            document.InsertTableOfContents("Programatically generated TOC", TableOfContentsSwitches.H);            
+        }
+
+        private static void CreateChapterWithBuletedList(DocX document)
+        {
+            // Add a paragraph at the end of the template.
+            var p1 = document.InsertParagraph("TECHNICAL SOLUTION");
+            p1.StyleName = "Heading1";
+
+            var p2 = document.InsertParagraph("Picture form Acusticus Module Configuration and/or from VISIO");
+            p2.StyleName = "No spacing";
+
+            var p3 = document.InsertParagraph("DESCRIPTION OF FUNCTIONALITIES");
+            p3.StyleName = "Heading2";
+
+            var p4 = document.InsertParagraph("text of description of functionalities");
+            p4.StyleName = "Normal";
+
+            var p5 = document.InsertParagraph("PARTS INCLUDED IN THE DELIVERY");
+            p5.StyleName = "Heading2";
+
+            var p6 = document.InsertParagraph("text for PARTS INCLUDED IN THE DELIVERY");
+            p6.StyleName = "Normal";
+
+            var p7 = document.InsertParagraph("PARTS EXCLUDED FROM THE DELIVERY");
+            p7.StyleName = "Heading2";
+
+            var bulletedList = document.AddList("Mast / pole", 0, ListItemType.Bulleted);
+            document.AddListItem(bulletedList, "Batteries");
+            document.AddListItem(bulletedList, "Aerials and coaxial cables");
+            // Add an item (level 0)
+            document.AddListItem(bulletedList, "Radio - stations");
+            document.AddListItem(bulletedList, "Radio station 1", 1);
+            document.AddListItem(bulletedList, "Radio station 2", 1);
+            document.AddListItem(bulletedList, "Push buttons");
+            document.AddListItem(bulletedList, "Olflex cables");
+            document.AddListItem(bulletedList, "CAN cables");
+            document.AddListItem(bulletedList, "Solar panels and accessories");
+
+            document.InsertList(bulletedList);
+        }
+
+        //Indentations
+        //// Add the first paragraph.
+        //var p = document.InsertParagraph("This is the first paragraph. It doesn't contain any indentation.");
+        //p.SpacingAfter(30);
+        //        // Add the second paragraph.
+        //        var p2 = document.InsertParagraph("This is the second paragraph. It contains an indentation on the first line.");
+        //// Indent only the first line of the Paragraph.
+        //p2.IndentationFirstLine = -1.0f;
+        //        p2.SpacingAfter(30);
+        //        // Add the third paragraph.
+        //        var p3 = document.InsertParagraph("This is the third paragraph. It contains an indentation on all the lines except the first one.");
+        //// Indent all the lines of the Paragraph, except the first.
+        //p3.IndentationHanging = 1.0f;
+
+    }
+}
+
