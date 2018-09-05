39269547c9fa31f32cd2d860a48f294c324c9648
ondro <ondro@ONDRO-PC>
2018-09-05 22:32:10 +0200

    PointF

---
+++ TOPO/BaseClasses/GraphObj/PointF.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClasses.GraphObj
+{
+    public class PointF
+    {
+        //----------------------------------------------------------------------------
+        float MX;
+        float MY;
+
+        //----------------------------------------------------------------------------
+        public float X
+        {
+            get
+            {
+                return MX;
+            }
+            set
+            {
+                MX = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return MY;
+            }
+
+            set
+            {
+                MY = value;
+            }
+        }
+
+        //----------------------------------------------------------------------------
+        //----------------------------------------------------------------------------
+        //----------------------------------------------------------------------------
+        public PointF(float x, float y)
+        {
+            MX = x;
+            MY = y;
+        }
+
+        
+    }
+}
