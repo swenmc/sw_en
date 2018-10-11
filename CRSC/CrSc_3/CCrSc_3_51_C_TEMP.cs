ce173a6b2d2f7f8c79f880322cf0347df7858997
swenmc <swenmc@gmail.com>
2018-10-12 09:47:01 +1300

    Upravy - obdlzniky namiesto  jednotlivych trojuholnikov

--- CRSC/CrSc_3/CCrSc_3_51_C_TEMP.cs
+++ CRSC/CrSc_3/CCrSc_3_51_C_TEMP.cs
@@ -90,7 +90,7 @@ namespace CRSC
         {
             // Fill Point Array Data in LCS (Local Coordinate System of Cross-Section, horizontal y, vertical - z)
 
-            // Defined Clockwise
+            // Defined Clockwise CW
             // Point No. 1
             CrScPointsOut.Add(new Point(b / 2.0, h / 2.0 - m_ft_f));
             // Point No. 2
@@ -111,19 +111,22 @@ namespace CRSC
 
         protected override void loadCrScIndicesFrontSide()
         {
-            //TriangleIndicesFrontSide = new Int32Collection(3 * 6);
+            TriangleIndicesFrontSide = new Int32Collection(3 * 6);
 
+            // zadavane v CCW - jednotlive trojuholniky
             TriangleIndicesFrontSide = new Int32Collection() { 0, 1, 6, 0, 6, 7, 1, 2, 6, 6, 2, 5, 5, 2, 4, 2, 3, 4 };
 
-            //AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 0, 1, 6, 7);
-            //AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 1, 2, 5, 6);
-            //AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 2, 3, 4, 5);
+            // zadavane v CCW - obdlzniky
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 0, 1, 6, 7);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 1, 2, 5, 6);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 2, 3, 4, 5);
         }
 
         protected override void loadCrScIndicesBackSide()
         {
-            TriangleIndicesBackSide = new Int32Collection();
+            TriangleIndicesBackSide = new Int32Collection(3 * 6);
 
+            // zadavane v CCW - jednotlive trojuholniky
             TriangleIndicesBackSide = new Int32Collection() { ITotNoPoints + 0, ITotNoPoints + 6, ITotNoPoints + 1,
                 ITotNoPoints + 0, ITotNoPoints + 7, ITotNoPoints + 6,
                 ITotNoPoints + 1, ITotNoPoints + 6, ITotNoPoints + 2,
@@ -131,16 +134,17 @@ namespace CRSC
                 ITotNoPoints + 5, ITotNoPoints + 4, ITotNoPoints + 2,
                 ITotNoPoints + 2, ITotNoPoints + 4, ITotNoPoints + 3 };
 
-            //AddRectangleIndices_CW_1234(TriangleIndicesBackSide, ITotNoPoints + 0, ITotNoPoints + 1, ITotNoPoints + 6, ITotNoPoints + 7);
-            //AddRectangleIndices_CW_1234(TriangleIndicesBackSide, ITotNoPoints + 1, ITotNoPoints + 2, ITotNoPoints + 5, ITotNoPoints + 6);
-            //AddRectangleIndices_CW_1234(TriangleIndicesBackSide, ITotNoPoints + 2, ITotNoPoints + 3, ITotNoPoints + 4, ITotNoPoints + 5);
+            // zadavane v CW - obdlzniky (normaly sa vo funkcii zmenia)
+            AddRectangleIndices_CW_1234(TriangleIndicesBackSide, ITotNoPoints + 0, ITotNoPoints + 1, ITotNoPoints + 6, ITotNoPoints + 7);
+            AddRectangleIndices_CW_1234(TriangleIndicesBackSide, ITotNoPoints + 1, ITotNoPoints + 2, ITotNoPoints + 5, ITotNoPoints + 6);
+            AddRectangleIndices_CW_1234(TriangleIndicesBackSide, ITotNoPoints + 2, ITotNoPoints + 3, ITotNoPoints + 4, ITotNoPoints + 5);
         }
 
         protected override void loadCrScIndicesShell()
         {
             TriangleIndicesShell = new Int32Collection();
 
-            // CCW
+            // zadavane v CCW - jednotlive trojuholniky
             TriangleIndicesShell = new Int32Collection() { 0,9,1, 0,8,9,
                                                            1,9,10, 1,10,2,
                                                            2,10,11, 2,11,3,
@@ -149,6 +153,16 @@ namespace CRSC
                                                            5,13,14, 5,14,6,
                                                            6,14,15, 6,15,7,
                                                            7,8,0, 7,15,8};
+
+            // zadavane v CCW - obdlzniky
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 0, 8, 9, 1);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 1, 9, 10, 2);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 2, 10, 11, 3);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 3, 11, 12, 4);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 4, 12, 13, 5);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 5, 13, 14, 6);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 6, 14, 15, 7);
+            AddRectangleIndices_CCW_1234(TriangleIndicesFrontSide, 7, 15, 8, 0);
         }
     }
 }
