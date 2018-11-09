ad71dd3c9a85830e9abfcaa086758db84853ae8e
swenmc <swenmc@gmail.com>
2018-11-09 16:01:21 +1300

    Nieco som pokazil :)

--- PFD/UC_Loads.xaml.cs
+++ PFD/UC_Loads.xaml.cs
@@ -26,7 +26,7 @@ namespace PFD
     {
         BuildingGeometryDataInput sGeometryInputData;
         public UC_Loads(BuildingGeometryDataInput geometryInputData)
-        {            
+        {
             InitializeComponent();
 
             sGeometryInputData = geometryInputData;
