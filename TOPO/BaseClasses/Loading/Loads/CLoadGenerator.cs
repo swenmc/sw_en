31a1e0a9917307371bd4b4a3643140cc126a2c3c
ondro <ondro@ONDRO-PC>
2019-03-05 21:47:57 +0100

    Member on SurfaceLoadPlane

--- TOPO/BaseClasses/Loading/Loads/CLoadGenerator.cs
+++ TOPO/BaseClasses/Loading/Loads/CLoadGenerator.cs
@@ -18,39 +18,39 @@ namespace BaseClasses
         //        m_arrLoadCases[01].MemberLoadsList.Add(new CMLoad_21(fMemberLoadValue, m, EMLoadTypeDistr.eMLT_FS_G_11, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
         //    }
 
-        public static void GenerateMemberLoads(CLoadCase[] m_arrLoadCases, List<CMember> members, float fDist)
-        {
-            foreach (CLoadCase lc in m_arrLoadCases)
-            {
-                int iLoadID_Temp = 1;
-                foreach (CSLoad_Free csload in lc.SurfaceLoadsList)
-                {
-                    if (csload is CSLoad_FreeUniform)
-                    {
-                        float fMemberLoadValue = ((CSLoad_FreeUniform)csload).fValue * fDist;
-                        foreach (CMember m in members)
-                        {
-                            lc.MemberLoadsList.Add(new CMLoad_21(iLoadID_Temp, fMemberLoadValue, m, EMLoadTypeDistr.eMLT_QUF_W_21, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
-                            iLoadID_Temp += 1;
-                        }
-                    }
-                    if (csload is CSLoad_FreeUniformGroup)
-                    {
-                        CSLoad_FreeUniformGroup group = (CSLoad_FreeUniformGroup)csload;
-                        foreach (CSLoad_FreeUniform csloadFree in group.LoadList)
-                        {
-                            float fMemberLoadValue = csloadFree.fValue * fDist;
-                            foreach (CMember m in members)
-                            {
-                                lc.MemberLoadsList.Add(new CMLoad_21(iLoadID_Temp, fMemberLoadValue, m, EMLoadTypeDistr.eMLT_QUF_W_21, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
-                                iLoadID_Temp += 1;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
+        //public static void GenerateMemberLoads(CLoadCase[] m_arrLoadCases, List<CMember> members, float fDist)
+        //{
+        //    foreach (CLoadCase lc in m_arrLoadCases)
+        //    {
+        //        int iLoadID_Temp = 1;
+        //        foreach (CSLoad_Free csload in lc.SurfaceLoadsList)
+        //        {
+        //            if (csload is CSLoad_FreeUniform)
+        //            {
+        //                float fMemberLoadValue = ((CSLoad_FreeUniform)csload).fValue * fDist;
+        //                foreach (CMember m in members)
+        //                {
+        //                    lc.MemberLoadsList.Add(new CMLoad_21(iLoadID_Temp, fMemberLoadValue, m, EMLoadTypeDistr.eMLT_QUF_W_21, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
+        //                    iLoadID_Temp += 1;
+        //                }
+        //            }
+        //            if (csload is CSLoad_FreeUniformGroup)
+        //            {
+        //                CSLoad_FreeUniformGroup group = (CSLoad_FreeUniformGroup)csload;
+        //                foreach (CSLoad_FreeUniform csloadFree in group.LoadList)
+        //                {
+        //                    float fMemberLoadValue = csloadFree.fValue * fDist;
+        //                    foreach (CMember m in members)
+        //                    {
+        //                        lc.MemberLoadsList.Add(new CMLoad_21(iLoadID_Temp, fMemberLoadValue, m, EMLoadTypeDistr.eMLT_QUF_W_21, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
+        //                        iLoadID_Temp += 1;
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
         //public static void GenerateMemberLoads(CLoadCase loadCase, List<CMember> members, float fDist)
         //{
         //    foreach (CSLoad_Free csload in loadCase.SurfaceLoadsList)
@@ -62,6 +62,77 @@ namespace BaseClasses
         //        }
         //    }
         //}
+
+
+        public static void GenerateMemberLoads(CLoadCase[] m_arrLoadCases, List<CMember> members, float fDist)
+        {
+            foreach (CLoadCase lc in m_arrLoadCases)
+            {
+                int iLoadID = 0;
+                int c = 0;
+                foreach (CSLoad_Free csload in lc.SurfaceLoadsList)
+                {
+                    c++;
+                    foreach (CMember m in members)
+                    {
+                        if (csload is CSLoad_FreeUniformGroup)
+                        {
+                            Transform3DGroup loadGroupTransform = ((CSLoad_FreeUniformGroup)csload).CreateTransformCoordGroupOfLoadGroup();
+                            foreach (CSLoad_FreeUniform l in ((CSLoad_FreeUniformGroup)csload).LoadList)
+                            {
+                                if (MemberLiesOnSurfaceLoadPlane(l, m, null)) System.Diagnostics.Trace.WriteLine($"LoadCase: {lc.Name} Surface: {c} contains member: {m.ID}");
+                                else { System.Diagnostics.Trace.WriteLine($"ERROR: Member {m.ID} not on plane. LoadCase: {lc.Name} Surface: {c}"); continue; }
+                                
+                                //ak by bol cely pod tou serface load
+                                float fMemberLoadValue = l.fValue * fDist;
+                                lc.MemberLoadsList.Add(new CMLoad_21(iLoadID, fMemberLoadValue, m, EMLoadTypeDistr.eMLT_QUF_W_21, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
+                                iLoadID += 1;
+                            }
+                        }
+                        else if (csload is CSLoad_FreeUniform)
+                        {
+                            CSLoad_FreeUniform l = (CSLoad_FreeUniform)csload;
+                            if(MemberLiesOnSurfaceLoadPlane(l, m, null)) System.Diagnostics.Trace.WriteLine($"LoadCase: {lc.Name} Surface: {c} contains member: {m.ID}");
+                            else { System.Diagnostics.Trace.WriteLine($"ERROR: Member {m.ID} not on plane. LoadCase: {lc.Name} Surface: {c}"); continue; }
+
+                            //ak by bol cely pod tou serface load
+                            float fMemberLoadValue = l.fValue * fDist;
+                            lc.MemberLoadsList.Add(new CMLoad_21(iLoadID, fMemberLoadValue, m, EMLoadTypeDistr.eMLT_QUF_W_21, EMLoadType.eMLT_F, EMLoadDirPCC1.eMLD_PCC_FZV_MYU, true, 0));
+                            iLoadID += 1;
+                        }
+
+                    } //foreach member
+                } //foreach surface load in load case
+            } //foreach loadcase
+        }
+
+
+        private static bool MemberLiesOnSurfaceLoadPlane(CSLoad_FreeUniform l, CMember m, Transform3DGroup loadGroupTransform)
+        {
+            l.PointsGCS = GetSurfaceLoadCoordinates_GCS(l, loadGroupTransform); // Positions in global coordinate system GCS
+            if (l.PointsGCS.Count < 2) { System.Diagnostics.Trace.WriteLine("ERROR in method MemberLiesOnSurfaceLoadPlane. Surface points collection must have 3 points. LoadID: " + l.ID); return false; }
+            
+            return Drawing3D.MemberLiesOnPlane(l.PointsGCS[0], l.PointsGCS[1], l.PointsGCS[2], m);
+        }
+
+        public static List<Point3D> GetSurfaceLoadCoordinates_GCS(CSLoad_FreeUniform load, Transform3D groupTransform)
+        {
+            Model3DGroup gr = load.CreateM_3D_G_Load();
+            if (gr.Children.Count < 1) return new List<Point3D>();
+
+            Transform3DGroup trans = new Transform3DGroup();
+            trans.Children.Add(gr.Transform);
+            if (groupTransform != null)
+            {
+                trans.Children.Add(groupTransform);
+            }
+
+            List<Point3D> transPoints = new List<Point3D>();
+            foreach (Point3D p in load.SurfaceDefinitionPoints)
+                transPoints.Add(trans.Transform(p));
+
+            return transPoints;
+        }
 
     }
 }
