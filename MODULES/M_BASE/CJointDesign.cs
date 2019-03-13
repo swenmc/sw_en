d494d5c80827883ab6b8452cb1b7ca2f86965145
swenmc <swenmc@gmail.com>
2019-03-13 14:26:19 +1300

    Refactoring - joint

--- MODULES/M_BASE/CJointDesign.cs
+++ MODULES/M_BASE/CJointDesign.cs
@@ -13,9 +13,7 @@ namespace M_BASE
     {
         bool bDebugging;
         public float fDesignRatio_Start = 0;
-
         public float fDesignRatio_End = 0;
-
         public bool BUseCRSCGeometricalAxes;
 
         public CJointDesign(bool bDebugging_temp = false)
@@ -41,63 +39,49 @@ namespace M_BASE
 
                     if (j == 0) // Start Joint Design
                     {
-                        // Internal forces in joint
-                        sjointStartDIF_x.fN = sBIF_x[j].fN;
-                        sjointStartDIF_x.fN_c = sjointStartDIF_x.fN > 0 ? 0f : Math.Abs(sjointStartDIF_x.fN);
-                        sjointStartDIF_x.fN_t = sjointStartDIF_x.fN < 0 ? 0f : sjointStartDIF_x.fN;
-                        sjointStartDIF_x.fT = sBIF_x[j].fT;
+                        // Set design internal forces in joint
+                        SetDesignInternalForces(bUseCRSCGeometricalAxes, sBIF_x[j], ref sjointStartDIF_x);
 
-                        if (bUseCRSCGeometricalAxes)
-                        {
-                            sjointStartDIF_x.fV_yy = sBIF_x[j].fV_yy;
-                            sjointStartDIF_x.fV_zz = sBIF_x[j].fV_zz;
-
-                            sjointStartDIF_x.fM_yy = sBIF_x[j].fM_yy;
-                            sjointStartDIF_x.fM_zz = sBIF_x[j].fM_zz;
-                        }
-                        else
-                        {
-                            sjointStartDIF_x.fV_yu = sBIF_x[j].fV_yu;
-                            sjointStartDIF_x.fV_zv = sBIF_x[j].fV_zv;
-
-                            sjointStartDIF_x.fM_yu = sBIF_x[j].fM_yu;
-                            sjointStartDIF_x.fM_zv = sBIF_x[j].fM_zv;
-                        }
-
-                        // Design
+                        // Design joint
                         obj_CalcDesign = new CCalculJoint(bDebugging, bUseCRSCGeometricalAxes, jointStart, sjointStartDIF_x);
                         fDesignRatio_Start = obj_CalcDesign.fEta_max;
                     }
                     else // End Joint Design
                     {
-                        // Internal forces in joint
-                        sjointEndDIF_x.fN = sBIF_x[j].fN;
-                        sjointEndDIF_x.fN_c = sjointEndDIF_x.fN > 0 ? 0f : Math.Abs(sjointEndDIF_x.fN);
-                        sjointEndDIF_x.fN_t = sjointEndDIF_x.fN < 0 ? 0f : sjointEndDIF_x.fN;
-                        sjointEndDIF_x.fT = sBIF_x[j].fT;
+                        // Set design internal forces in joint
+                        SetDesignInternalForces(bUseCRSCGeometricalAxes, sBIF_x[j], ref sjointEndDIF_x);
 
-                        if (bUseCRSCGeometricalAxes)
-                        {
-                            sjointEndDIF_x.fV_yy = sBIF_x[j].fV_yy;
-                            sjointEndDIF_x.fV_zz = sBIF_x[j].fV_zz;
-
-                            sjointEndDIF_x.fM_yy = sBIF_x[j].fM_yy;
-                            sjointEndDIF_x.fM_zz = sBIF_x[j].fM_zz;
-                        }
-                        else
-                        {
-                            sjointEndDIF_x.fV_yu = sBIF_x[j].fV_yu;
-                            sjointEndDIF_x.fV_zv = sBIF_x[j].fV_zv;
-
-                            sjointEndDIF_x.fM_yu = sBIF_x[j].fM_yu;
-                            sjointEndDIF_x.fM_zv = sBIF_x[j].fM_zv;
-                        }
-
-                        // Design
+                        // Design joint
                         obj_CalcDesign = new CCalculJoint(bDebugging, bUseCRSCGeometricalAxes, jointEnd, sjointEndDIF_x);
                         fDesignRatio_End = obj_CalcDesign.fEta_max;
                     }
                 }
+            }
+        }
+
+        private void SetDesignInternalForces(bool bUseCRSCGeometricalAxes, basicInternalForces sBIF_x, ref designInternalForces sDIF_x)
+        {
+            // Internal forces
+            sDIF_x.fN = sBIF_x.fN;
+            sDIF_x.fN_c = sDIF_x.fN > 0 ? 0f : Math.Abs(sDIF_x.fN);
+            sDIF_x.fN_t = sDIF_x.fN < 0 ? 0f : sDIF_x.fN;
+            sDIF_x.fT = sBIF_x.fT;
+
+            if (bUseCRSCGeometricalAxes)
+            {
+                sDIF_x.fV_yy = sBIF_x.fV_yy;
+                sDIF_x.fV_zz = sBIF_x.fV_zz;
+
+                sDIF_x.fM_yy = sBIF_x.fM_yy;
+                sDIF_x.fM_zz = sBIF_x.fM_zz;
+            }
+            else
+            {
+                sDIF_x.fV_yu = sBIF_x.fV_yu;
+                sDIF_x.fV_zv = sBIF_x.fV_zv;
+
+                sDIF_x.fM_yu = sBIF_x.fM_yu;
+                sDIF_x.fM_zv = sBIF_x.fM_zv;
             }
         }
     }
