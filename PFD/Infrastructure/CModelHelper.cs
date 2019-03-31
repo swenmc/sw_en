93170ff7bf2f36d3dd349c059470c2de01fba5e7
ondro <ondro@ONDRO-PC>
2019-03-31 21:30:53 +0200

    236 funguje zobrazovanie skryvanie members okrem doors a windows

--- PFD/Infrastructure/CModelHelper.cs
+++ PFD/Infrastructure/CModelHelper.cs
@@ -400,7 +400,7 @@ namespace PFD
                         count++;
                         // Assign component properties from GUI component list to the particular members in the model
                         m.BIsGenerated = cInfo.Generate;
-                        m.BIsDisplayed = cInfo.Display;
+                        m.BIsDisplayed = cInfo.Display; //nastavenie zobrazenia pre dany member
                         m.BIsSelectedForIFCalculation = cInfo.Calculate;
                         m.BIsSelectedForDesign = cInfo.Design;
                         m.BIsSelectedForMaterialList = cInfo.MaterialList;
