234a921447fa9ceebce5ef02dcc20bbbae4a4b09
swenmc <swenmc@gmail.com>
2019-04-04 21:09:49 +1300

    Database Joints - in work

---
+++ DATABASE/CJointsManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Globalization;
+using DATABASE.DTO;
+
+namespace DATABASE
+{
+    public static class CJointsManager
+    {
+
+    }
+}
