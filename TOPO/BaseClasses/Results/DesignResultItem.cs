68e099a42cb483d44fbd16890db40d312adf2fc9
ondro <777ondro@gmail.com>
2019-05-04 11:35:12 +0200

    DesignResultItem

---
+++ TOPO/BaseClasses/Results/DesignResultItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClasses
+{    
+    public class DesignResultItem
+    {
+        private float m_MaximumDesignRatio;
+        private CLoadCombination m_GoverningLoadCombination;
+        private CMember m_MemberWithMaximumDesignRatio;
+
+        public float MaximumDesignRatio
+        {
+            get
+            {
+                return m_MaximumDesignRatio;
+            }
+
+            set
+            {
+                m_MaximumDesignRatio = value;
+            }
+        }
+
+        public CLoadCombination GoverningLoadCombination
+        {
+            get
+            {
+                return m_GoverningLoadCombination;
+            }
+
+            set
+            {
+                m_GoverningLoadCombination = value;
+            }
+        }
+
+        public CMember MemberWithMaximumDesignRatio
+        {
+            get
+            {
+                return m_MemberWithMaximumDesignRatio;
+            }
+
+            set
+            {
+                m_MemberWithMaximumDesignRatio = value;
+            }
+        }
+
+        public DesignResultItem() { }
+        public DesignResultItem(float maximumDesignRatio, CLoadCombination governingLoadCombination, CMember memberWithMaximumDesignRatio)
+        {
+            m_MaximumDesignRatio = maximumDesignRatio;
+            m_GoverningLoadCombination = governingLoadCombination;
+            m_MemberWithMaximumDesignRatio = memberWithMaximumDesignRatio;
+        }
+
+
+    }
+}
