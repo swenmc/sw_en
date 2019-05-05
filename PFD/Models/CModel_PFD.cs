bdd6cd84458b322b6a83e973c524317f0b4806a2
swenmc <swenmc@gmail.com>
2019-05-05 13:26:44 +1200

    Uprava

--- PFD/Models/CModel_PFD.cs
+++ PFD/Models/CModel_PFD.cs
@@ -7,21 +7,6 @@ using BaseClasses;
 
 namespace PFD
 {
-    enum EMemberGroupNames
-    {
-        eMainColumn = 0,    // Internal frame
-        eRafter = 1,        // Internal frame
-        eMainColumn_EF = 2, // Edge frame
-        eRafter_EF = 3,     // Edge frame
-        eEavesPurlin = 4,
-        eGirtWall = 5,
-        ePurlin = 6,
-        eFrontColumn = 7,
-        eBackColumn = 8,
-        eFrontGirt = 9,
-        eBackGirt = 10
-    }
-
     public class CModel_PFD : CExample
     {
         public List<CEntity3D> componentList;
