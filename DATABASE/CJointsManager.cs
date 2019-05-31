ef74ad404937040264889e5a4204899964ccee78
ondro <777ondro@gmail.com>
2019-05-31 11:31:44 +0200

    DB - JointsManager

--- DATABASE/CJointsManager.cs
+++ DATABASE/CJointsManager.cs
@@ -4,11 +4,34 @@ using System.Configuration;
 using System.Data.SQLite;
 using System.Globalization;
 using DATABASE.DTO;
+using MATERIAL;
 
 namespace DATABASE
 {
     public static class CJointsManager
     {
+        public static List<CConnectionDescription> LoadJointsConnectionDescriptions()
+        {
+            CConnectionDescription item;
+            List<CConnectionDescription> items = new List<CConnectionDescription>();
 
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["JointsSQLiteDB"].ConnectionString))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand("Select * from connectionDescription", conn);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        item = new CConnectionDescription();
+                        item.ID = reader.GetInt32(reader.GetOrdinal("ID"));
+                        item.Name = reader["name"].ToString();
+                        item.JoinType = reader["jointType"].ToString();
+                        items.Add(item);
+                    }
+                }
+            }
+            return items;
+        }
     }
 }
---
+++ DATABASE/DTO/CConnectionDescription.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATABASE.DTO
+{
+    public class CConnectionDescription
+    {
+        private int m_ID;
+        private string m_Name;
+        private string m_JoinType;
+        
+        public int ID
+        {
+            get
+            {
+                return m_ID;
+            }
+
+            set
+            {
+                m_ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+
+            set
+            {
+                m_Name = value;
+            }
+        }
+
+        public string JoinType
+        {
+            get
+            {
+                return m_JoinType;
+            }
+
+            set
+            {
+                m_JoinType = value;
+            }
+        }
+
+        public CConnectionDescription() { }
+    }
+}
