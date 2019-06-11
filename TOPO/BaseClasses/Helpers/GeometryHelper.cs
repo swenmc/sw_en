c5e722917d9fee421983ef2e6ce7caacfe75130e
ondro <777ondro@gmail.com>
2019-06-11 08:49:31 +0200

    FindDistanceToSegment

---
+++ TOPO/BaseClasses/Helpers/GeometryHelper.cs
@@ -0,0 +1,62 @@
+﻿using BaseClasses.GraphObj;
+using DATABASE;
+using DATABASE.DTO;
+using MATERIAL;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BaseClasses.Helpers
+{
+    public static class GeometryHelper
+    {
+        // Calculate the distance between
+        // point pt and the segment p1 --> p2.
+        public static double FindDistanceToSegment(
+            PointF pt, PointF p1, PointF p2, out PointF closest)
+        {
+            float dx = p2.X - p1.X;
+            float dy = p2.Y - p1.Y;
+            if ((dx == 0) && (dy == 0))
+            {
+                // It's a point not a line segment.
+                closest = p1;
+                dx = pt.X - p1.X;
+                dy = pt.Y - p1.Y;
+                return Math.Sqrt(dx * dx + dy * dy);
+            }
+
+            // Calculate the t that minimizes the distance.
+            float t = ((pt.X - p1.X) * dx + (pt.Y - p1.Y) * dy) /
+                (dx * dx + dy * dy);
+
+            // See if this represents one of the segment's
+            // end points or a point in the middle.
+            if (t < 0)
+            {
+                closest = new PointF(p1.X, p1.Y);
+                dx = pt.X - p1.X;
+                dy = pt.Y - p1.Y;
+            }
+            else if (t > 1)
+            {
+                closest = new PointF(p2.X, p2.Y);
+                dx = pt.X - p2.X;
+                dy = pt.Y - p2.Y;
+            }
+            else
+            {
+                closest = new PointF(p1.X + t * dx, p1.Y + t * dy);
+                dx = pt.X - closest.X;
+                dy = pt.Y - closest.Y;
+            }
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+    }
+}
