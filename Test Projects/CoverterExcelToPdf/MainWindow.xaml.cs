440ca5f7b8fa2cc5f01d51b2eec97e97e3fa357b
ondro <777ondro@gmail.com>
2019-08-01 08:36:17 +0200

    Converter do PDF ukladanie dokumentu Result.pdf

--- Test Projects/CoverterExcelToPdf/MainWindow.xaml.cs
+++ Test Projects/CoverterExcelToPdf/MainWindow.xaml.cs
@@ -26,105 +26,7 @@ namespace CoverterExcelToPdf
         {
             InitializeComponent();
         }
-
-        //private void BtnExportToPDFFromDirectory_Click(object sender, RoutedEventArgs e)
-        //{
-        //    using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
-        //    {
-        //        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-        //        {
-        //            WaitWindow ww = new WaitWindow("PDF");
-        //            ww.Show();
-
-        //            string folder = dialog.SelectedPath;
-        //            DirectoryInfo dirInfo = new DirectoryInfo(folder);
-        //            FileInfo[] files = dirInfo.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly);
-        //            if (files.Length == 0) { ww.Close(); MessageBox.Show("No .xlsx files in the directory.");  return; }
-
-
-
-        //            using (ExcelPackage resultPackage = new ExcelPackage())
-        //            {
-        //                //ExcelWorksheet resultSheet = resultPackage.Workbook.Worksheets.Add("Sheet 1");
-        //                int count = 1;
-
-        //                foreach (FileInfo fi in files)
-        //                {
-        //                    using (ExcelPackage pckg = new ExcelPackage(fi))
-        //                    {
-        //                        var sheet = pckg.Workbook.Worksheets.FirstOrDefault();
-        //                        if (sheet == null) continue;
-
-        //                        //add new sheet
-        //                        resultPackage.Workbook.Worksheets.Add("Sheet " + count, sheet);
-        //                    }
-
-
-
-        //                    //for (var r = 1; r <= sheet.Dimension.Rows; r++)
-        //                    //{
-        //                    //    for (var c = 1; c <= sheet.Dimension.Columns; c++)
-        //                    //    {
-        //                    //        var cellSrc = sheet.Cells[r, c];
-        //                    //        var cellDest = resultSheet.Cells[r, c];
-
-        //                    //        // Copy value
-        //                    //        cellDest.Value = cellSrc.Value;
-
-        //                    //        // Copy cell properties
-        //                    //        cellDest.Style.Numberformat = cellSrc.Style.Numberformat;
-        //                    //        cellDest.Style.Font.Bold = cellSrc.Style.Font.Bold;
-        //                    //        // TODO... Add any additional properties that you may want to copy over
-        //                    //    }
-        //                    //}
-
-
-        //                    //foreach (var sheet in pckg.Workbook.Worksheets)
-        //                    //{
-        //                    //    //check name of worksheet, in case that worksheet with same name already exist exception will be thrown by EPPlus
-        //                    //    string workSheetName = sheet.Name;
-        //                    //    foreach (var masterSheet in resultPackage.Workbook.Worksheets)
-        //                    //    {
-        //                    //        if (sheet.Name == masterSheet.Name)
-        //                    //        {
-        //                    //            workSheetName = string.Format("{0}_{1}", workSheetName, DateTime.Now.ToString("yyyyMMddhhssmmm"));
-        //                    //        }
-        //                    //    }
-
-        //                    //    //add new sheet
-        //                    //    masterPackage.Workbook.Worksheets.Add(workSheetName, sheet);
-        //                    //}
-        //                    count++;
-
-        //                }
-
-
-        //                string fileName = string.Format("{0}\\{1}", folder, "ExportAll.pdf");
-        //                string fileNameXLSX = string.Format("{0}\\{1}", folder, "ExportAll.xlsx");
-        //                //ExportToExcelDocument.ExportToExcel(fileNameXLSX, tableParams, "plates");
-        //                //CExportToPDF.SavePDFDocument(fileName);
-
-        //                try
-        //                {
-        //                    using (FileStream fs = File.OpenWrite(fileNameXLSX))
-        //                    {
-        //                        resultPackage.SaveAs(fs);
-        //                    }
-        //                }
-        //                catch (IOException ex)
-        //                {
-        //                    // The process cannot access the file 'filename' because it is being used by another process
-        //                    MessageBox.Show("The process cannot access the file because it is being used by another process.");
-        //                    return;
-        //                }
-
-        //            }
-
-        //        }
-        //    }
-        //}
-
-
+        
         private void BtnExportToPDFFromDirectory_Click(object sender, RoutedEventArgs e)
         {
             ExcelPackage resultPackage = new ExcelPackage();
@@ -143,7 +45,7 @@ namespace CoverterExcelToPdf
                     FileInfo[] files = dirInfo.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly);
                     if (files.Length == 0) { ww.Close(); MessageBox.Show("No .xlsx files in the directory."); return; }
 
-                    SpreadsheetInfo.SetLicense("FREE - LIMITED - KEY");
+                    //SpreadsheetInfo.SetLicense("FREE - LIMITED - KEY");
 
                     FileInfo databaseFile = files.FirstOrDefault(f => f.Name.Contains("DATABASE.xlsx"));
                     //if (databaseFile != null) Process.Start(databaseFile.FullName); // Otvaram Satabase file ako workbook v Exceli, preto som toto zakomentoval
@@ -160,29 +62,16 @@ namespace CoverterExcelToPdf
                     {
                         if (!fi.Extension.Equals(".xlsx")) continue;
                         if (fi.Name.Equals(databaseFile.Name)) continue;
-
-                        //1
-                        //ExcelFile excel = ExcelFile.Load(fi.FullName);
-                        //excel.Save(fi.FullName.Substring(0, fi.FullName.Length - 5) +".pdf");
-
-                        //2
-                        //tato kniznica vie zase exportovat len tabulky, pokial je tam prvy riadok prazdny,tak ma problem
-                        //ExcelToPdfReport r = new ExcelToPdfReport();
-                        //r.CreateExcelToPdfReport(fi.FullName, null);
-
-                        //3
+                        
                         try
                         {
                             Workbook wkb = app.Workbooks.Open(fi.FullName);
                             var firstSheeet = wkb.Sheets[1];
 
-                            //for (int i = 2; i < wkb.Sheets.Count; i++) // To Ondrej - podla mna nemozes zmazat ostatne zalozky, zmazes si tak data, na ktorych je zavisly obsah prvej, upravil som to tak ze sa exportuje len prvy zosit
-                            //{
-                            //    wkb.Sheets[i].Delete();
-                            //}
-
                             // Export only first worrksheet, not whole workbook
                             firstSheeet.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, fi.FullName.Substring(0, fi.FullName.Length - 5) + ".pdf");
+
+                            wkb.Close(false);
                         }
                         catch (Exception ex) {}
                     }
@@ -194,7 +83,7 @@ namespace CoverterExcelToPdf
 
                     // Close Excel
                     app.Quit();
-
+                    
                     MergePDFDocuments(folder);
 
                     ww.Close();
@@ -245,8 +134,9 @@ namespace CoverterExcelToPdf
             }
             
             // Save the document...
-            string filename = "Result.pdf";
+            string filename = directory + "\\Result.pdf";
             outputDocument.Save(filename);
+            
             // ...and start a viewer.
             Process.Start(filename);
         }
