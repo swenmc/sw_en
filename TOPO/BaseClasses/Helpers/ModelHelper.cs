2e2dd6aee596c40d9d4b4bb85c5df7caf08e3bf3
swenmc <sw.en.mc@gmail.com>
2019-08-19 19:18:11 +1200

    Filtre

--- TOPO/BaseClasses/Helpers/ModelHelper.cs
+++ TOPO/BaseClasses/Helpers/ModelHelper.cs
@@ -7,7 +7,9 @@ namespace BaseClasses.Helpers
 {
     public static class ModelHelper
     {
-
+        // TO Ondrej
+        // TODO - zapracoval som vseobecnejsie funkcie  GetMembersInDistanceInterval a GetNodesInDistanceInterval
+        // Mali by fungovat pre rozne smery, takze nimi mozno nahradit niektore z nizsie uvedenych funkcii kde sa porovnavaju specificke suradnice uzlov natvrdo napr. m.NodeStart.Y
         public static Point3D GetPoint3D(this CNode node)
         {
             return new Point3D(node.X, node.Y, node.Z);
@@ -15,78 +17,42 @@ namespace BaseClasses.Helpers
 
         public static CMember[] GetFrontViewMembers(CModel model)
         {
-            List<CMember> members = new List<CMember>();
-
-            foreach (CMember m in model.m_arrMembers)
-            {
-                if (MathF.d_equal(m.NodeStart.Y, 0) && MathF.d_equal(m.NodeEnd.Y, 0)) members.Add(m);
-            }
-            return members.ToArray();
+            return GetMembersInDistance(model, 0, 1); // smer Y
         }
 
         public static CNode[] GetFrontViewNodes(CModel model)
         {
-            List<CNode> nodes = new List<CNode>();
-
-            foreach (CNode n in model.m_arrNodes)
-            {
-                if (MathF.d_equal(n.Y, 0)) nodes.Add(n);
-            }
-            return nodes.ToArray();
+            return GetNodesInDistance(model, 0, 1); // smer Y
         }
 
         public static CMember[] GetBackViewMembers(CModel model)
         {
-            return GetMembersInYDistance(model, model.fL_tot);
+            return GetMembersInDistance(model, model.fL_tot, 1); // smer Y
         }
 
         public static CNode[] GetBackViewNodes(CModel model)
         {
-            return GetNodesInYDistance(model, model.fL_tot);
+            return GetNodesInDistance(model, model.fL_tot, 1); // smer Y
         }
 
         public static CMember[] GetLeftViewMembers(CModel model)
         {
-            List<CMember> members = new List<CMember>();
-
-            foreach (CMember m in model.m_arrMembers)
-            {
-                if (MathF.d_equal(m.NodeStart.X, 0) && MathF.d_equal(m.NodeEnd.X, 0)) members.Add(m);
-            }
-            return members.ToArray();
+            return GetMembersInDistance(model, 0, 0); // smer X
         }
 
         public static CNode[] GetLeftViewNodes(CModel model)
         {
-            List<CNode> nodes = new List<CNode>();
-
-            foreach (CNode n in model.m_arrNodes)
-            {
-                if (MathF.d_equal(n.X, 0)) nodes.Add(n);
-            }
-            return nodes.ToArray();
+            return GetNodesInDistance(model, 0, 0); // smer X
         }
 
         public static CMember[] GetRightViewMembers(CModel model)
         {
-            List<CMember> members = new List<CMember>();
-
-            foreach (CMember m in model.m_arrMembers)
-            {
-                if (MathF.d_equal(m.NodeStart.X, model.fW_frame) && MathF.d_equal(m.NodeEnd.X, model.fW_frame)) members.Add(m);
-            }
-            return members.ToArray();
+            return GetMembersInDistance(model, model.fW_frame, 0); // smer X
         }
 
         public static CNode[] GetRightViewNodes(CModel model)
         {
-            List<CNode> nodes = new List<CNode>();
-
-            foreach (CNode n in model.m_arrNodes)
-            {
-                if (MathF.d_equal(n.X, model.fW_frame)) nodes.Add(n);
-            }
-            return nodes.ToArray();
+            return GetNodesInDistance(model, model.fW_frame, 0); // smer X
         }
 
         // Roof
@@ -156,75 +122,87 @@ namespace BaseClasses.Helpers
         // Intermediate frame (main frame - 2nd gridline)
         public static CMember[] GetMiddleFrameMembers(CModel model, bool bAddGirtsAndPurlins = true)
         {
+            int iDirectionCode = 1; // iDirectionCode 0- direction X, 1-direction Y, 2 - direction Z
+
             if (!bAddGirtsAndPurlins)
-                return GetMembersInYDistance(model, model.fL1_frame);
+                return GetMembersInDistance(model, model.fL1_frame, iDirectionCode);
             else
-                return GetMembersInYDistanceInterval(model, 0, model.fL1_frame, false, true, true);
+                return GetMembersInDistanceInterval(model, 0, model.fL1_frame, iDirectionCode, false, true, true);
         }
 
         public static CNode[] GetMiddleFrameNodes(CModel model, bool bAddGirtsAndPurlins = true)
         {
+            int iDirectionCode = 1; // iDirectionCode 0- direction X, 1-direction Y, 2 - direction Z
+
             if (!bAddGirtsAndPurlins)
-                return GetNodesInYDistance(model, model.fL1_frame);
+                return GetNodesInDistance(model, model.fL1_frame, iDirectionCode);
             else
-                return GetNodesInYDistanceInterval(model, 0, model.fL1_frame, false, true);
+                return GetNodesInDistanceInterval(model, 0, model.fL1_frame, iDirectionCode, false, true);
         }
 
-        public static CMember[] GetMembersInYDistance(CModel model, double length)
+        public static CMember[] GetMembersInDistance(CModel model, double coordinate, int iDirectionCode)
         {
             List<CMember> members = new List<CMember>();
 
             foreach (CMember m in model.m_arrMembers)
             {
-                if (MathF.d_equal(m.NodeStart.Y, length) && MathF.d_equal(m.NodeEnd.Y, length)) members.Add(m);
+                float fNodeStartCoordinate = SetNodeCoordinateForSpecificDirection(m.NodeStart, iDirectionCode);
+                float fNodeEndCoordinate = SetNodeCoordinateForSpecificDirection(m.NodeEnd, iDirectionCode);
+
+                if (MathF.d_equal(fNodeStartCoordinate, coordinate) && MathF.d_equal(fNodeEndCoordinate, coordinate)) members.Add(m);
             }
             return members.ToArray();
         }
 
-        public static CNode[] GetNodesInYDistance(CModel model, double length)
+        public static CNode[] GetNodesInDistance(CModel model, double coordinate, int iDirectionCode)
         {
             List<CNode> nodes = new List<CNode>();
 
             foreach (CNode n in model.m_arrNodes)
             {
-                if (MathF.d_equal(n.Y, length)) nodes.Add(n);
+                float fNodeCoordinate = SetNodeCoordinateForSpecificDirection(n, iDirectionCode);
+                if (MathF.d_equal(fNodeCoordinate, coordinate)) nodes.Add(n);
             }
             return nodes.ToArray();
         }
 
-        public static CMember[] GetMembersInYDistanceInterval(CModel model, double startPositionY, double endPositionY, bool bIncludingStart = false, bool bIncludingEnd = false, bool bIncludingPartial = true)
+        public static CMember[] GetMembersInDistanceInterval(CModel model, double startPosition, double endPosition, int iDirectionCode, bool bIncludingStart = false, bool bIncludingEnd = false, bool bIncludingPartial = true)
         {
+            // iDirectionCode 0- direction X, 1-direction Y, 2 - direction Z
             // TODO - dopracovat selekciu podla typu pruta
 
             float fLimit = 0.0001f; // Limit pre uzatvoreny interval (0.1 mm)
 
-            if (bIncludingStart) startPositionY -= fLimit;
-            if (bIncludingEnd) endPositionY += fLimit;
+            if (bIncludingStart) startPosition -= fLimit;
+            if (bIncludingEnd) endPosition += fLimit;
 
             List<CMember> members = new List<CMember>();
 
             foreach (CMember m in model.m_arrMembers)
             {
+                float fNodeStartCoordinate = SetNodeCoordinateForSpecificDirection(m.NodeStart, iDirectionCode);
+                float fNodeEndCoordinate = SetNodeCoordinateForSpecificDirection(m.NodeEnd, iDirectionCode);
+
                 if (!bIncludingPartial) // Selektuje len pruty ktore su cele v otvorenom alebo uzavretom intervale
                 {
-                    if ((!bIncludingStart && !bIncludingEnd) && (m.NodeStart.Y > startPositionY && m.NodeEnd.Y > startPositionY) && (m.NodeStart.Y < endPositionY && m.NodeEnd.Y < endPositionY)) // Whole member in interval - otvoreny interval
+                    if ((!bIncludingStart && !bIncludingEnd) && (fNodeStartCoordinate > startPosition && fNodeEndCoordinate > startPosition) && (fNodeStartCoordinate < endPosition && fNodeEndCoordinate < endPosition)) // Whole member in interval - otvoreny interval
                         members.Add(m);
-                    else if (bIncludingStart && ((m.NodeStart.Y >= startPositionY && m.NodeEnd.Y >= startPositionY) && (m.NodeStart.Y < endPositionY && m.NodeEnd.Y < endPositionY)))
+                    else if (bIncludingStart && ((fNodeStartCoordinate >= startPosition && fNodeEndCoordinate >= startPosition) && (fNodeStartCoordinate < endPosition && fNodeEndCoordinate < endPosition)))
                         members.Add(m);
-                    else if (bIncludingEnd && ((m.NodeStart.Y > startPositionY && m.NodeEnd.Y > startPositionY) && (m.NodeStart.Y <= endPositionY && m.NodeEnd.Y <= endPositionY)))
+                    else if (bIncludingEnd && ((fNodeStartCoordinate > startPosition && fNodeEndCoordinate > startPosition) && (fNodeStartCoordinate <= endPosition && fNodeEndCoordinate <= endPosition)))
                         members.Add(m);
-                    else if ((bIncludingStart && bIncludingEnd) && ((m.NodeStart.Y >= startPositionY && m.NodeEnd.Y >= startPositionY) && (m.NodeStart.Y <= endPositionY && m.NodeEnd.Y <= endPositionY))) // Uzavrety interval
+                    else if ((bIncludingStart && bIncludingEnd) && ((fNodeStartCoordinate >= startPosition && fNodeEndCoordinate >= startPosition) && (fNodeStartCoordinate <= endPosition && fNodeEndCoordinate <= endPosition))) // Uzavrety interval
                         members.Add(m);
                 }
                 else // Selektuje pruty ktore maju aspon jeden uzol v intervale
                 {
-                    if ((!bIncludingStart && !bIncludingEnd) && ((m.NodeStart.Y > startPositionY || m.NodeEnd.Y > startPositionY) && (m.NodeStart.Y < endPositionY || m.NodeEnd.Y < endPositionY))) // Some node in interval - otvoreny interval
+                    if ((!bIncludingStart && !bIncludingEnd) && ((fNodeStartCoordinate > startPosition || fNodeEndCoordinate > startPosition) && (fNodeStartCoordinate < endPosition || fNodeEndCoordinate < endPosition))) // Some node in interval - otvoreny interval
                         members.Add(m);
-                    else if (bIncludingStart && ((m.NodeStart.Y >= startPositionY || m.NodeEnd.Y >= startPositionY) && (m.NodeStart.Y < endPositionY || m.NodeEnd.Y < endPositionY)))
+                    else if (bIncludingStart && ((fNodeStartCoordinate >= startPosition || fNodeEndCoordinate >= startPosition) && (fNodeStartCoordinate < endPosition || fNodeEndCoordinate < endPosition)))
                         members.Add(m);
-                    else if (bIncludingEnd && ((m.NodeStart.Y > startPositionY || m.NodeEnd.Y > startPositionY) && (m.NodeStart.Y <= endPositionY || m.NodeEnd.Y <= endPositionY)))
+                    else if (bIncludingEnd && ((fNodeStartCoordinate > startPosition || fNodeEndCoordinate > startPosition) && (fNodeStartCoordinate <= endPosition || fNodeEndCoordinate <= endPosition)))
                         members.Add(m);
-                    else if ((bIncludingStart && bIncludingEnd) && ((m.NodeStart.Y >= startPositionY || m.NodeEnd.Y >= startPositionY) && (m.NodeStart.Y <= endPositionY || m.NodeEnd.Y <= endPositionY))) // Uzavrety interval
+                    else if ((bIncludingStart && bIncludingEnd) && ((fNodeStartCoordinate >= startPosition || fNodeEndCoordinate >= startPosition) && (fNodeStartCoordinate <= endPosition || fNodeEndCoordinate <= endPosition))) // Uzavrety interval
                         members.Add(m);
                 }
             }
@@ -232,30 +210,42 @@ namespace BaseClasses.Helpers
             return members.ToArray();
         }
 
-        public static CNode[] GetNodesInYDistanceInterval(CModel model, double startPositionY, double endPositionY, bool bIncludingStart = false, bool bIncludingEnd = false/*, bool bIncludingPartial = true*/)
+        public static CNode[] GetNodesInDistanceInterval(CModel model, double startPosition, double endPosition, int iDirectionCode, bool bIncludingStart = false, bool bIncludingEnd = false/*, bool bIncludingPartial = true*/)
         {
-            // TODO - dopracovat selekciu podla typu pruta
-
             float fLimit = 0.0001f; // Limit pre uzatvoreny interval (0.1 mm)
 
-            if (bIncludingStart) startPositionY -= fLimit;
-            if (bIncludingEnd) endPositionY += fLimit;
+            if (bIncludingStart) startPosition -= fLimit;
+            if (bIncludingEnd) endPosition += fLimit;
 
             List<CNode> nodes = new List<CNode>();
 
             foreach (CNode n in model.m_arrNodes)
             {
-                if ((!bIncludingStart && !bIncludingEnd) && (n.Y > startPositionY && n.Y < endPositionY)) // Node in interval - otvoreny interval
+                float fNodeCoordinate = SetNodeCoordinateForSpecificDirection(n, iDirectionCode);
+
+                if ((!bIncludingStart && !bIncludingEnd) && (fNodeCoordinate > startPosition && fNodeCoordinate < endPosition)) // Node in interval - otvoreny interval
                     nodes.Add(n);
-                else if (bIncludingStart && (n.Y >= startPositionY && n.Y < endPositionY))
+                else if (bIncludingStart && (fNodeCoordinate >= startPosition && fNodeCoordinate < endPosition))
                     nodes.Add(n);
-                else if (bIncludingEnd && (n.Y > startPositionY && n.Y <= endPositionY))
+                else if (bIncludingEnd && (fNodeCoordinate > startPosition && fNodeCoordinate <= endPosition))
                     nodes.Add(n);
-                else if ((bIncludingStart && bIncludingEnd) && (n.Y >= startPositionY && n.Y <= endPositionY)) // Uzavrety interval
+                else if ((bIncludingStart && bIncludingEnd) && (fNodeCoordinate >= startPosition && fNodeCoordinate <= endPosition)) // Uzavrety interval
                     nodes.Add(n);
             }
 
             return nodes.ToArray();
+        }
+
+        private static float SetNodeCoordinateForSpecificDirection(CNode n, int iDirectionCode)
+        {
+            // Funckia vrati suradnicu uzla pre specificky smer GCS ktory chceme uvazovat
+
+            if(iDirectionCode == 0) // Direction X
+                return n.X;
+            if (iDirectionCode == 1) // Direction Y
+                return n.Y;
+            else //if (iDirectionCode == 2) // Direction ZX
+                return n.Z;
         }
     }
 }
