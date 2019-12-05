83f4a09595c2cc5b0afa34137c8ba295b1b1a599
swenmc <sw.en.mc@gmail.com>
2019-12-06 10:16:01 +1300

    Washers a Opravy

---
+++ DATABASE/CWashersManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Globalization;
+using DATABASE.DTO;
+using MATERIAL;
+
+namespace DATABASE
+{
+    public static class CWashersManager
+    {
+        // Rectangular Washers W
+
+        public static List<CRectWasher_W_Properties> LoadPlate_W_Descriptions()
+        {
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+
+            CRectWasher_W_Properties item;
+            List<CRectWasher_W_Properties> items = new List<CRectWasher_W_Properties>();
+
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["WashersSQLiteDB"].ConnectionString))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand("Select * from Washers", conn);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        item = GetPlate_W_Properties(reader);
+                        items.Add(item);
+                    }
+                }
+            }
+            return items;
+        }
+
+        public static List<string> GetPlateLL_Names()
+        {
+            List<CRectWasher_W_Properties> items = LoadPlate_W_Descriptions();
+
+            List<string> names = new List<string>();
+
+            foreach (CRectWasher_W_Properties item in items)
+                names.Add(item.Name);
+
+            return names;
+        }
+
+        public static CRectWasher_W_Properties GetPlate_W_Properties(int id)
+        {
+            CRectWasher_W_Properties properties = null;
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["WashersSQLiteDB"].ConnectionString))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand("Select * from Washers WHERE ID = @id", conn);
+                command.Parameters.AddWithValue("@id", id);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        properties = GetPlate_W_Properties(reader);
+                    }
+                }
+            }
+            return properties;
+        }
+
+        public static CRectWasher_W_Properties GetPlate_W_Properties(string name)
+        {
+            CRectWasher_W_Properties properties = null;
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["WashersSQLiteDB"].ConnectionString))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand("Select * from Washers WHERE washerName = @name", conn);
+                command.Parameters.AddWithValue("@name", name);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        properties = GetPlate_W_Properties(reader);
+                    }
+                }
+            }
+            return properties;
+        }
+
+        private static CRectWasher_W_Properties GetPlate_W_Properties(SQLiteDataReader reader)
+        {
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+
+            CRectWasher_W_Properties properties = new CRectWasher_W_Properties();
+            properties.ID = reader.GetInt32(reader.GetOrdinal("ID"));
+            properties.Name = reader["plateName"].ToString();
+            properties.dim1x = double.Parse(reader["dim1x"].ToString(), nfi) / 1000;
+            properties.dim2y = double.Parse(reader["dim2y"].ToString(), nfi) / 1000;
+            properties.thickness = double.Parse(reader["t"].ToString(), nfi) / 1000;
+            properties.NumberOfHoles = Int32.Parse(reader["iNumberHoles"].ToString());
+            properties.Area = double.Parse(reader["area"].ToString(), nfi) / 1000000;
+            properties.Volume = double.Parse(reader["volume"].ToString(), nfi) / 1000000000;
+            properties.Mass = double.Parse(reader["mass"].ToString(), nfi);
+            properties.Price_PPSM_NZD = double.Parse(reader["price_PPSM_NZD"].ToString(), nfi);
+            properties.Price_PPKG_NZD = double.Parse(reader["price_PPKG_NZD"].ToString(), nfi);
+            properties.Price_PPP_NZD = double.Parse(reader["price_PPP_NZD"].ToString(), nfi);
+
+            return properties;
+        }
+    }
+}
---
+++ DATABASE/DTO/CRectWasher_W_Properties.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATABASE.DTO
+{
+    public class CRectWasher_W_Properties
+    {
+        private int m_ID;
+        private string m_Name;
+        private double m_dim1x;
+        private double m_dim2y;
+        private double m_thickness;
+        private int m_iNumberOfHoles;
+        private double m_area;
+        private double m_volume;
+        private double m_mass;
+        private double m_price_PPSM_NZD;
+        private double m_price_PPKG_NZD;
+        private double m_price_PPP_NZD;
+
+        public int ID
+        {
+            get
+            {
+                return m_ID;
+            }
+
+            set
+            {
+                m_ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+
+            set
+            {
+                m_Name = value;
+            }
+        }
+
+        public double dim1x
+        {
+            get
+            {
+                return m_dim1x;
+            }
+
+            set
+            {
+                m_dim1x = value;
+            }
+        }
+
+        public double dim2y
+        {
+            get
+            {
+                return m_dim2y;
+            }
+
+            set
+            {
+                m_dim2y = value;
+            }
+        }
+
+        public double thickness
+        {
+            get
+            {
+                return m_thickness;
+            }
+
+            set
+            {
+                m_thickness = value;
+            }
+        }
+
+        public int NumberOfHoles
+        {
+            get
+            {
+                return m_iNumberOfHoles;
+            }
+
+            set
+            {
+                m_iNumberOfHoles = value;
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return m_area;
+            }
+
+            set
+            {
+                m_area = value;
+            }
+        }
+
+        public double Volume
+        {
+            get
+            {
+                return m_volume;
+            }
+
+            set
+            {
+                m_volume = value;
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return m_mass;
+            }
+
+            set
+            {
+                m_mass = value;
+            }
+        }
+
+        public double Price_PPSM_NZD
+        {
+            get
+            {
+                return m_price_PPSM_NZD;
+            }
+
+            set
+            {
+                m_price_PPSM_NZD = value;
+            }
+        }
+
+        public double Price_PPKG_NZD
+        {
+            get
+            {
+                return m_price_PPKG_NZD;
+            }
+
+            set
+            {
+                m_price_PPKG_NZD = value;
+            }
+        }
+
+        public double Price_PPP_NZD
+        {
+            get
+            {
+                return m_price_PPP_NZD;
+            }
+
+            set
+            {
+                m_price_PPP_NZD = value;
+            }
+        }
+
+        public CRectWasher_W_Properties() { }
+    }
+}
