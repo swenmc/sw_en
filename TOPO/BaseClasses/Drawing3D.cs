135f371fcf6f57da9458b7f9fb70b15b74fdd90e
ondro <ondro@ONDRO-PC>
2020-01-17 09:58:16 +0100

    ...

--- TOPO/BaseClasses/Drawing3D.cs
+++ TOPO/BaseClasses/Drawing3D.cs
@@ -2377,7 +2377,8 @@ namespace BaseClasses
 
                     for (int i = 0; i < wireFramePoints.Count / 2; i++)
                     {
-                        float fFactor = 1f;
+                        //float fFactor = 1f; toto som menil pre task 477
+                        float fFactor = 0.01f;
                         GeometryModel3D cylinder = Get3DLineReplacement(sDiplayOptions.wireFrameColor, fFactor * sDiplayOptions.fWireFrameLineThickness, wireFramePoints[i * 2], wireFramePoints[i * 2 + 1]);
                         cylinders.Children.Add(cylinder);
                     }
@@ -2593,7 +2594,8 @@ namespace BaseClasses
                     {
                         //To Mato - to je co toto za factor? nema sa brat nahodou podla velkosti modelu,alebo take nieco?
                         // To Ondrej - to je taky faktor co prevada hodnotu line thickness v pointoch na metre ako rozmer valca pre 3D, takze napriklad 2 na 0.02 m
-                        float fFactor = 1.1f; //bolo 0.01f
+                        //float fFactor = 1.1f; //bolo 0.01f 
+                        float fFactor = 0.01f; //bolo 0.01f  toto treba zriesit pre PDF treba faktor podla velkosti modelu ale pre word treba taky faktor aby bolo vidno wireframe dobre
                         GeometryModel3D cylinder = Get3DLineReplacement(sDisplayOptions.wireFrameColor, fFactor * sDisplayOptions.fWireFrameLineThickness, wireFramePoints[i * 2], wireFramePoints[i * 2 + 1]);
                         cylinders.Children.Add(cylinder);
                     }
