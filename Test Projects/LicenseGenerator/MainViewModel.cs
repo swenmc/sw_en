c7c1de00b167863a1183f8e8df5cedb8321f5652
ondro <ondro@ONDRO-PC>
2020-03-16 23:38:52 +0100

    License geenrator project

---
+++ Test Projects/LicenseGenerator/MainViewModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace LicenseGenerator
+{
+    [Serializable]
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        public event PropertyChangedEventHandler PropertyChanged;
+        //-------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------
+        private string m_CustomerName;
+        private List<string> m_Roles;
+        private string m_Role;
+        private int m_Year;
+        private int m_Month;
+        private int m_Day;
+        private string m_key;
+
+        public bool IsSetFromCode = false;
+                
+        //-------------------------------------------------------------------------------------------------------------
+        public string CustomerName
+        {
+            get
+            {
+                return m_CustomerName;
+            }
+
+            set
+            {
+                m_CustomerName = value;
+                NotifyPropertyChanged("CustomerName");
+            }
+        }
+
+        public List<string> Roles
+        {
+            get
+            {
+                if (m_Roles == null) m_Roles = new List<string>() { "developer", "admin", "customer", "architect", "engineer", "salesperson"};
+                return m_Roles;
+            }
+
+            set
+            {
+                m_Roles = value;
+                Role = m_Roles.First();
+                NotifyPropertyChanged("Roles");
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return m_Role;
+            }
+
+            set
+            {
+                m_Role = value;
+                NotifyPropertyChanged("Role");
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return m_Year;
+            }
+
+            set
+            {
+                m_Year = value;
+                NotifyPropertyChanged("Year");
+            }
+        }
+
+        public int Month
+        {
+            get
+            {
+                return m_Month;
+            }
+
+            set
+            {
+                m_Month = value;
+                NotifyPropertyChanged("Month");
+            }
+        }
+
+        public int Day
+        {
+            get
+            {
+                return m_Day;
+            }
+
+            set
+            {
+                m_Day = value;
+                NotifyPropertyChanged("Day");
+            }
+        }
+
+        public string Key
+        {
+            get
+            {
+                return m_key;
+            }
+
+            set
+            {
+                m_key = value;
+                NotifyPropertyChanged("Key");
+            }
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------
+        public MainViewModel()
+        {
+            DateTime trial = DateTime.Now.AddDays(30);
+            Year = trial.Year;
+            Month = trial.Month;
+            Day = trial.Day;
+
+            IsSetFromCode = false;
+        }
+
+        public string GetLicenseKey()
+        {
+            string text = $"{Year}_{Month}_{Day}_{Role}";
+            string passPhrase = $"ompc_{CustomerName}";
+            string encryptedstring = StringCipher.Encrypt(text, passPhrase);
+
+            string decryptedstring = StringCipher.Decrypt(encryptedstring, passPhrase);
+
+            return encryptedstring;
+        }
+
+        //-------------------------------------------------------------------------------------------------------------
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Test Projects/LicenseGenerator/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LicenseGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            vm = new MainViewModel();
+            vm.PropertyChanged += Vm_PropertyChanged;
+            this.DataContext = vm;
+        }
+
+        private void Vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Key = vm.GetLicenseKey();
+        }
+
+        private void Button(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(vm.Key);
+        }
+    }
+}
