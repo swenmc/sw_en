8956b7713297b4173b227e86d088f251efb9a6ea
swenmc <sw.en.mc@gmail.com>
2020-04-06 10:34:51 +1200

    Uprava screwarrangement LL

--- TOPO/BaseClasses/Model/ConnectionComponents/ConnectorArrangement/CScrewArrangement_LL.cs
+++ TOPO/BaseClasses/Model/ConnectionComponents/ConnectorArrangement/CScrewArrangement_LL.cs
@@ -51,29 +51,49 @@ namespace BaseClasses
 
                     HolesCentersPoints2D[7] = new Point(HolesCentersPoints2D[6].X, HolesCentersPoints2D[1].Y);
 
-                    float fTemp = (0.5f * fbX1 + 0.5f * flZ);
+                    if (MATH.MathF.d_equal(fbX1, flZ)) // Sirka bX1 a lZ su rovnake, skrutky len posunieme v smere Y of fTemp
+                    {
+                        float fTemp = (0.5f * fbX1 + 0.5f * flZ);
 
-                    HolesCentersPoints2D[8] = new Point(HolesCentersPoints2D[7].X, fTemp + HolesCentersPoints2D[7].Y);
+                        HolesCentersPoints2D[8] = new Point(HolesCentersPoints2D[7].X, fTemp + HolesCentersPoints2D[7].Y);
 
-                    HolesCentersPoints2D[9] = new Point(HolesCentersPoints2D[6].X, fTemp + HolesCentersPoints2D[6].Y);
+                        HolesCentersPoints2D[9] = new Point(HolesCentersPoints2D[6].X, fTemp + HolesCentersPoints2D[6].Y);
 
-                    HolesCentersPoints2D[10] = new Point(HolesCentersPoints2D[5].X, fTemp + HolesCentersPoints2D[5].Y);
+                        HolesCentersPoints2D[10] = new Point(HolesCentersPoints2D[5].X, fTemp + HolesCentersPoints2D[5].Y);
 
-                    HolesCentersPoints2D[11] = new Point(HolesCentersPoints2D[4].X, fTemp + HolesCentersPoints2D[4].Y);
+                        HolesCentersPoints2D[11] = new Point(HolesCentersPoints2D[4].X, fTemp + HolesCentersPoints2D[4].Y);
 
-                    HolesCentersPoints2D[12] = new Point(HolesCentersPoints2D[3].X, fTemp + HolesCentersPoints2D[3].Y);
+                        HolesCentersPoints2D[12] = new Point(HolesCentersPoints2D[3].X, fTemp + HolesCentersPoints2D[3].Y);
 
-                    HolesCentersPoints2D[13] = new Point(HolesCentersPoints2D[2].X, fTemp + HolesCentersPoints2D[2].Y);
+                        HolesCentersPoints2D[13] = new Point(HolesCentersPoints2D[2].X, fTemp + HolesCentersPoints2D[2].Y);
 
-                    HolesCentersPoints2D[14] = new Point(HolesCentersPoints2D[0].X, fTemp + HolesCentersPoints2D[1].Y);
+                        HolesCentersPoints2D[14] = new Point(HolesCentersPoints2D[0].X, fTemp + HolesCentersPoints2D[1].Y);
 
-                    HolesCentersPoints2D[15] = new Point(HolesCentersPoints2D[1].X, fTemp + HolesCentersPoints2D[0].Y);
+                        HolesCentersPoints2D[15] = new Point(HolesCentersPoints2D[1].X, fTemp + HolesCentersPoints2D[0].Y);
+                    }
+                    else // Sirka bX1 a lZ nie su rovnake, vypocitame nove pozicie tak aby boli vzdialenosti od okrajov rovnake
+                    {
+                        HolesCentersPoints2D[8] = new Point(HolesCentersPoints2D[7].X, fbX1 + flZ - fx_edge);
+
+                        HolesCentersPoints2D[9] = new Point(HolesCentersPoints2D[6].X, fbX1 + fx_edge);
+
+                        HolesCentersPoints2D[10] = new Point(HolesCentersPoints2D[5].X, fbX1 + 0.5f * flZ);
+
+                        HolesCentersPoints2D[11] = new Point(HolesCentersPoints2D[4].X, fbX1 + 0.5f * flZ);
+
+                        HolesCentersPoints2D[12] = new Point(HolesCentersPoints2D[3].X, fbX1 + 0.5f * flZ);
+
+                        HolesCentersPoints2D[13] = new Point(HolesCentersPoints2D[2].X, fbX1 + 0.5f * flZ);
+
+                        HolesCentersPoints2D[14] = new Point(HolesCentersPoints2D[0].X, fbX1 + flZ - fx_edge);
+
+                        HolesCentersPoints2D[15] = new Point(HolesCentersPoints2D[1].X, fbX1 + fx_edge);
+                    }
 
                     for (int i = 0; i < IHolesNumber / 2; i++)
                     {
                         HolesCentersPoints2D[IHolesNumber / 2 + i] =
                             new Point(fbX2 - HolesCentersPoints2D[(IHolesNumber / 2 - i - 1)].X, HolesCentersPoints2D[(IHolesNumber / 2 - i - 1)].Y);
-
                     }
                 }
                 else
