26bb5521d939380b1829b7105271bd98b32c92ba
sw_en <sw.en.mc@gmail.com>
2020-09-30 15:49:09 +0200

    Krasne textury

--- TOPO/BaseClasses/GraphObj/Areas/CAreaPolygonal.cs
+++ TOPO/BaseClasses/GraphObj/Areas/CAreaPolygonal.cs
@@ -19,7 +19,7 @@ namespace BaseClasses.GraphObj
 
         public CAreaPolygonal()
         {
-        
+
         }
 
         // Constructor 2
@@ -38,95 +38,43 @@ namespace BaseClasses.GraphObj
             FTime = fTime;
         }
 
-        public GeometryModel3D CreateArea(bool useTextures, bool vertical, DiffuseMaterial material)
+        public GeometryModel3D CreateArea(bool useTextures, DiffuseMaterial material)
         {
             if (m_EdgePointList == null) return null;
 
             MeshGeometry3D mesh = new MeshGeometry3D();
 
-            foreach( Point3D p in m_EdgePointList)
-               mesh.Positions.Add(p);
+            foreach (Point3D p in m_EdgePointList)
+                mesh.Positions.Add(p);
 
             for (int i = 0; i < m_EdgePointList.Count - 1; i++)
             {
-               mesh.TriangleIndices.Add(0); // Vsetky trojuholniky zacinaju v nule, uvazujeme konvexny polygon
-               mesh.TriangleIndices.Add(i + 1);
-               mesh.TriangleIndices.Add(i + 2);
+                mesh.TriangleIndices.Add(0); // Vsetky trojuholniky zacinaju v nule, uvazujeme konvexny polygon
+                mesh.TriangleIndices.Add(i + 1);
+                mesh.TriangleIndices.Add(i + 2);
             }
 
             if (useTextures)
             {
-                //vertical
-                if (vertical)
+                if (m_EdgePointList.Count == 3 || m_EdgePointList.Count == 4)
                 {
-                    if (m_EdgePointList.Count == 4)
-                    {
-                        mesh.TextureCoordinates.Add(new Point(0, 0));
-                        mesh.TextureCoordinates.Add(new Point(1, 0));
-                        mesh.TextureCoordinates.Add(new Point(1, 1));
-                        mesh.TextureCoordinates.Add(new Point(0, 1));
-                    }
-                    else
-                    {
-                        mesh.TextureCoordinates.Add(new Point(0, 1));
-                        mesh.TextureCoordinates.Add(new Point(1, 1));
-                        mesh.TextureCoordinates.Add(new Point(1, 0));
-                        mesh.TextureCoordinates.Add(new Point(0, 0));
-
-                        //mesh.TextureCoordinates.Add(new Point(0, 1));
-                        //mesh.TextureCoordinates.Add(new Point(1, 1));
-                        //mesh.TextureCoordinates.Add(new Point(1, 1 - (m_EdgePointList[2].Z / m_EdgePointList[3].Z)));
-
-                        //mesh.TextureCoordinates.Add(new Point(0, 1));
-                        //mesh.TextureCoordinates.Add(new Point(1, 1 - (m_EdgePointList[2].Z / m_EdgePointList[3].Z)));
-                        //mesh.TextureCoordinates.Add(new Point(0.5, 0));
-
-                        //mesh.TextureCoordinates.Add(new Point(0, 1));
-                        //mesh.TextureCoordinates.Add(new Point(0.5, 0));
-                        //mesh.TextureCoordinates.Add(new Point(0, 1 - (m_EdgePointList[2].Z / m_EdgePointList[3].Z)));
-
-                        /*
-                        mesh.TextureCoordinates.Add(new Point(0, 0));
-                        mesh.TextureCoordinates.Add(new Point(1, 0));
-                        mesh.TextureCoordinates.Add(new Point(1, 1));
-                        //mesh.TextureCoordinates.Add(new Point(0, 1));
-
-                        mesh.TextureCoordinates.Add(new Point(0, 0));
-                        mesh.TextureCoordinates.Add(new Point(1, m_EdgePointList[2].Z / m_EdgePointList[3].Z));
-                        mesh.TextureCoordinates.Add(new Point(0.5, 1));
-                        //mesh.TextureCoordinates.Add(new Point(0, 1));
-
-                        mesh.TextureCoordinates.Add(new Point(0, 0));
-                        mesh.TextureCoordinates.Add(new Point(0.5, 1));
-                        mesh.TextureCoordinates.Add(new Point(0, 1));
-                        //mesh.TextureCoordinates.Add(new Point(0, 1));
-                        */
-                    }
-                }
-                else
-                {
-                    //horizontal
                     mesh.TextureCoordinates.Add(new Point(0, 0));
                     mesh.TextureCoordinates.Add(new Point(1, 0));
                     mesh.TextureCoordinates.Add(new Point(1, 1));
                     mesh.TextureCoordinates.Add(new Point(0, 1));
                 }
-
-
-                //for (int i = 0; i < mesh.Positions.Count; i = i + 3)
-                //{
-                //    mesh.TextureCoordinates.Add(new Point(0, 0));
-                //    mesh.TextureCoordinates.Add(new Point(0, 1));
-                //    mesh.TextureCoordinates.Add(new Point(1, 1));
-                //}
-
-                /*
-                for (int i = 0; i < mesh.Positions.Count; i = i + 3)
+                else if(m_EdgePointList.Count == 5)
                 {
-                    mesh.TextureCoordinates.Add(new Point(0, 0));
-                    mesh.TextureCoordinates.Add(new Point(1, 0));
+                    mesh.TextureCoordinates.Add(new Point(0, 1));
                     mesh.TextureCoordinates.Add(new Point(1, 1));
-                }*/
+                    mesh.TextureCoordinates.Add(new Point(1, 1 - (m_EdgePointList[2].Z / m_EdgePointList[3].Z)));
+                    mesh.TextureCoordinates.Add(new Point(0.5, 0));
+                    mesh.TextureCoordinates.Add(new Point(0, 1 - (m_EdgePointList[2].Z / m_EdgePointList[3].Z)));
+                }
+                else
+                {
+                    throw new Exception("Not implemented.");
+                }
             }
 
             return new GeometryModel3D(mesh, material);
--- TOPO/BaseClasses/GraphObj/CCladding.cs
+++ TOPO/BaseClasses/GraphObj/CCladding.cs
@@ -17,6 +17,9 @@ namespace BaseClasses.GraphObj
         double claddingThickness_Wall = 0.030; // Dopracovat napojenie z databazy cladding MDBTrapezoidalSheeting - vlastnost height_m v tabulkach tableSections_m alebo trapezoidalSheeting_m
         double claddingThickness_Roof = 0.060; // Dopracovat napojenie z databazy cladding MDBTrapezoidalSheeting - vlastnost height_m
 
+        double claddingWidthRibModular_Wall = 0.190; // m // Dopracovat napojenie z databazy cladding MDBTrapezoidalSheeting widthRib_m
+        double claddingWidthRibModular_Roof = 0.300; // m // Dopracovat napojenie z databazy cladding MDBTrapezoidalSheeting widthRib_m
+
         double column_crsc_z_plus;
         double column_crsc_y_minus;
         double column_crsc_y_plus;
@@ -45,47 +48,6 @@ namespace BaseClasses.GraphObj
 
             Model3DGroup model_gr = new Model3DGroup();
 
-            DiffuseMaterial material_Wall = new DiffuseMaterial(new SolidColorBrush(colorWall)); // TODO Ondrej - nastavitelna farba pre zobrazenie v GUI a pre Export
-            DiffuseMaterial material_Roof = new DiffuseMaterial(new SolidColorBrush(colorRoof)); // TODO Ondrej - nastavitelna farba pre zobrazenie v GUI a pre Export
-
-            if (bUseTextures)
-            {
-                //var image = new Image { Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/wallTexture_PermanentGreen.jpg", UriKind.RelativeOrAbsolute)) };
-                //RenderOptions.SetCachingHint(image, CachingHint.Cache);
-                //RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
-                //material_Wall = new DiffuseMaterial(new VisualBrush(image));
-
-                //image = new Image { Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/roofTexture_Gold.jpg", UriKind.RelativeOrAbsolute)) };
-                //material_Roof = new DiffuseMaterial(new VisualBrush(image));
-
-                //materialGroup.Children.Add(material);
-                //model.BackMaterial = material;
-
-
-                
-                ImageBrush brush = new ImageBrush();
-                brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/wallTexture_PermanentGreen.jpg", UriKind.RelativeOrAbsolute));
-                brush.TileMode = TileMode.Tile;
-                brush.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;                
-                brush.Stretch = Stretch.UniformToFill;
-                brush.Viewport = new System.Windows.Rect(0, 0, 0.05, 0.05);                
-                material_Wall = new DiffuseMaterial(brush);
-                               
-                
-                brush = new ImageBrush();
-                brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/roofTexture_Gold.jpg", UriKind.RelativeOrAbsolute));
-                brush.TileMode = TileMode.Tile;
-                brush.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
-                //brush.ViewboxUnits = BrushMappingMode.Absolute;
-                brush.Stretch = Stretch.UniformToFill;
-                brush.Viewport = new System.Windows.Rect(0, 0, 0.05, 0.05);
-                //brush.AlignmentX = AlignmentX.Left;
-                //brush.AlignmentY = AlignmentY.Top;
-                //brush.Viewport = new System.Windows.Rect(0, 0, brush.ImageSource.Width, brush.ImageSource.Height);
-                material_Roof = new DiffuseMaterial(brush);
-            }
-
-
             // Vytvorime model v GCS [0,0,0] je uvazovana v bode m_ControlPoint
 
             double bottomEdge_z = 0;
@@ -102,46 +64,87 @@ namespace BaseClasses.GraphObj
 
             // Cladding Edges
 
+            Point3D pfront0_baseleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, column_crsc_y_minus, bottomEdge_z);
+            Point3D pfront1_baseright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, column_crsc_y_minus, bottomEdge_z);
+
+            Point3D pback0_baseleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, bottomEdge_z);
+            Point3D pback1_baseright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, bottomEdge_z);
+
+            DiffuseMaterial material_SideWall = new DiffuseMaterial(new SolidColorBrush(colorWall)); // TODO Ondrej - nastavitelna farba pre zobrazenie v GUI a pre Export
+            DiffuseMaterial material_FrontBackWall = new DiffuseMaterial(new SolidColorBrush(colorWall)); // TODO Ondrej - nastavitelna farba pre zobrazenie v GUI a pre Export
+            DiffuseMaterial material_Roof = new DiffuseMaterial(new SolidColorBrush(colorRoof)); // TODO Ondrej - nastavitelna farba pre zobrazenie v GUI a pre Export
+
+            if (bUseTextures)
+            {
+                // TODO Ondrej - potrebujeme nejako prepocitat faktor pre pomer absolutnej a relativnej velkosti
+                // Nejako som to urobil prosim o kontrolu (pripadne dorobit alternativu pre absolutne zadavanie)
+
+                // Obrazok textury ma a = 213.9 x 213.9 mm. velkost v pixeloch je c = 800 x 800 pxs, 3.74 pxs / mm
+                // Povedzme ze osova vzialenost rebier plechu je b = 130 mm
+                // Ak chceme na budove dlzky 10000 mm vykreslit 77 vln (textur), tak bude velkost strany Rect 130/10000
+                // Ak chceme mat vlny (sekvencie textur) rovnako velke bez ohladu na rozmery musime pouzit absolutne vykreslovanie alebo pre kazdu plochu (ktora ma inak dlhu zakladnu pre kreslenie textury)
+                // vypocitat specificky pomer
+
+                ImageBrush brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/wallTexture_PermanentGreen.jpg", UriKind.RelativeOrAbsolute));
+                brush.TileMode = TileMode.Tile;
+                brush.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+                brush.Stretch = Stretch.UniformToFill;
+                double rectSize = claddingWidthRibModular_Wall / (pfront1_baseright.X - pfront0_baseleft.X);
+                brush.Viewport = new System.Windows.Rect(0, 0, rectSize, rectSize);
+                material_FrontBackWall = new DiffuseMaterial(brush);
+
+                brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/wallTexture_PermanentGreen.jpg", UriKind.RelativeOrAbsolute));
+                brush.TileMode = TileMode.Tile;
+                brush.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+                brush.Stretch = Stretch.UniformToFill;
+                rectSize = claddingWidthRibModular_Wall / (pback0_baseleft.Y - pfront0_baseleft.Y);
+                brush.Viewport = new System.Windows.Rect(0, 0, rectSize, rectSize);
+                material_SideWall = new DiffuseMaterial(brush);
+
+                brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/Textures/roofTexture_Gold.jpg", UriKind.RelativeOrAbsolute));
+                brush.TileMode = TileMode.Tile;
+                brush.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+                brush.Stretch = Stretch.UniformToFill;
+                rectSize = claddingWidthRibModular_Roof / (pback0_baseleft.Y - pfront0_baseleft.Y);
+                brush.Viewport = new System.Windows.Rect(0, 0, rectSize, rectSize); // Rozmer v smere Y pre side wall a strechu nemusi byt rovnaky ak sa pouzije iny plech
+                material_Roof = new DiffuseMaterial(brush);
+            }
+
             if (eModelType == EModelType_FS.eKitsetMonoRoofEnclosed)
             {
                 // Monopitch Roof
 
-                Point3D pfront0_baseleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, column_crsc_y_minus, bottomEdge_z);
-                Point3D pfront1_baseright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, column_crsc_y_minus, bottomEdge_z);
                 Point3D pfront2_heightright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, column_crsc_y_minus, height_2_final);
                 Point3D pfront3_heightleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, column_crsc_y_minus, height_1_final);
 
-                Point3D pback0_baseleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, bottomEdge_z);
-                Point3D pback1_baseright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, bottomEdge_z);
                 Point3D pback2_heightright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, height_2_final);
                 Point3D pback3_heightleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, height_1_final);
 
                 if (bUseTextures)
                 {
                     // Front Wall
-                    model_gr.Children.Add(new CAreaPolygonal(0, new List<Point3D>() { pfront0_baseleft, pfront1_baseright, pfront2_heightright, pfront3_heightleft }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(0, new List<Point3D>() { pfront0_baseleft, pfront1_baseright, pfront2_heightright, pfront3_heightleft }, 0).CreateArea(bUseTextures, material_FrontBackWall));
                     // Back Wall
-                    model_gr.Children.Add(new CAreaPolygonal(1, new List<Point3D>() { pback1_baseright, pback0_baseleft, pback3_heightleft, pback2_heightright }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(1, new List<Point3D>() { pback1_baseright, pback0_baseleft, pback3_heightleft, pback2_heightright }, 0).CreateArea(bUseTextures, material_FrontBackWall));
                     // Left Wall
-                    model_gr.Children.Add(new CAreaPolygonal(2, new List<Point3D>() { pback0_baseleft, pfront0_baseleft, pfront3_heightleft, pback3_heightleft }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(2, new List<Point3D>() { pback0_baseleft, pfront0_baseleft, pfront3_heightleft, pback3_heightleft }, 0).CreateArea(bUseTextures, material_SideWall));
                     // Right Wall
-                    model_gr.Children.Add(new CAreaPolygonal(3, new List<Point3D>() { pfront1_baseright, pback1_baseright, pback2_heightright, pfront2_heightright }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(3, new List<Point3D>() { pfront1_baseright, pback1_baseright, pback2_heightright, pfront2_heightright }, 0).CreateArea(bUseTextures, material_SideWall));
                     // Roof
-                    model_gr.Children.Add(new CAreaPolygonal(4, new List<Point3D>() { pback3_heightleft, pfront3_heightleft, pfront2_heightright, pback2_heightright }, 0).CreateArea(bUseTextures, false, material_Roof));
+                    model_gr.Children.Add(new CAreaPolygonal(4, new List<Point3D>() { pfront2_heightright, pback2_heightright, pback3_heightleft, pfront3_heightleft }, 0).CreateArea(bUseTextures, material_Roof));
                 }
             }
             else if (eModelType == EModelType_FS.eKitsetGableRoofEnclosed)
             {
                 // Gable Roof
 
-                Point3D pfront0_baseleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, column_crsc_y_minus, bottomEdge_z);
-                Point3D pfront1_baseright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, column_crsc_y_minus, bottomEdge_z);
                 Point3D pfront2_heightright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, column_crsc_y_minus, height_1_final);
                 Point3D pfront3_heightleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, column_crsc_y_minus, height_1_final);
                 Point3D pfront4_top = new Point3D(0.5 * sBuildingGeomInputData.fW, column_crsc_y_minus, height_2_final);
 
-                Point3D pback0_baseleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, bottomEdge_z);
-                Point3D pback1_baseright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, bottomEdge_z);
                 Point3D pback2_heightright = new Point3D(sBuildingGeomInputData.fW + column_crsc_z_plus + claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, height_1_final);
                 Point3D pback3_heightleft = new Point3D(-column_crsc_z_plus - claddingThickness_Wall, sBuildingGeomInputData.fL + column_crsc_y_plus, height_1_final);
                 Point3D pback4_top = new Point3D(0.5 * sBuildingGeomInputData.fW, sBuildingGeomInputData.fL + column_crsc_y_plus, height_2_final);
@@ -149,17 +152,17 @@ namespace BaseClasses.GraphObj
                 if (bUseTextures)
                 {
                     // Front Wall
-                    model_gr.Children.Add(new CAreaPolygonal(0, new List<Point3D>() { pfront0_baseleft, pfront1_baseright, pfront2_heightright, pfront4_top, pfront3_heightleft }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(0, new List<Point3D>() { pfront0_baseleft, pfront1_baseright, pfront2_heightright, pfront4_top, pfront3_heightleft }, 0).CreateArea(bUseTextures, material_FrontBackWall));
                     // Back Wall
-                    model_gr.Children.Add(new CAreaPolygonal(1, new List<Point3D>() { pback1_baseright, pback0_baseleft, pback3_heightleft, pback4_top, pback2_heightright }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(1, new List<Point3D>() { pback1_baseright, pback0_baseleft, pback3_heightleft, pback4_top, pback2_heightright }, 0).CreateArea(bUseTextures, material_FrontBackWall));
                     // Left Wall
-                    model_gr.Children.Add(new CAreaPolygonal(2, new List<Point3D>() { pback0_baseleft, pfront0_baseleft, pfront3_heightleft, pback3_heightleft }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(2, new List<Point3D>() { pback0_baseleft, pfront0_baseleft, pfront3_heightleft, pback3_heightleft }, 0).CreateArea(bUseTextures, material_SideWall));
                     // Right Wall
-                    model_gr.Children.Add(new CAreaPolygonal(3, new List<Point3D>() { pfront1_baseright, pback1_baseright, pback2_heightright, pfront2_heightright }, 0).CreateArea(bUseTextures, true, material_Wall));
+                    model_gr.Children.Add(new CAreaPolygonal(3, new List<Point3D>() { pfront1_baseright, pback1_baseright, pback2_heightright, pfront2_heightright }, 0).CreateArea(bUseTextures, material_SideWall));
                     // Roof - Left Side
-                    model_gr.Children.Add(new CAreaPolygonal(4, new List<Point3D>() { pback3_heightleft, pfront3_heightleft, pfront4_top, pback4_top }, 0).CreateArea(bUseTextures, false, material_Roof));
+                    model_gr.Children.Add(new CAreaPolygonal(4, new List<Point3D>() { pfront4_top, pback4_top, pback3_heightleft, pfront3_heightleft }, 0).CreateArea(bUseTextures, material_Roof));
                     // Roof - Right Side
-                    model_gr.Children.Add(new CAreaPolygonal(5, new List<Point3D>() { pfront2_heightright, pback2_heightright, pback4_top, pfront4_top }, 0).CreateArea(bUseTextures, false, material_Roof));
+                    model_gr.Children.Add(new CAreaPolygonal(5, new List<Point3D>() { pfront2_heightright, pback2_heightright, pback4_top, pfront4_top }, 0).CreateArea(bUseTextures, material_Roof));
                 }
             }
             else
