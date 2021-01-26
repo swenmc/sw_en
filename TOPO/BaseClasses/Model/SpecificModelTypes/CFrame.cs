417666f9e4ff36fbfc3d8982f4fe23384e299769
ondro <ondro@ONDRO-PC>
2021-01-26 11:41:59 +0100

    Frame SetMaterialAndCrossSection

--- TOPO/BaseClasses/Model/SpecificModelTypes/CFrame.cs
+++ TOPO/BaseClasses/Model/SpecificModelTypes/CFrame.cs
@@ -34,25 +34,45 @@ namespace BaseClasses
         {
             if (m_arrMembers == null) return;
             if (m_arrMembers.Length < 2) return;
-            // Materials
-            // Materials List - Materials Array - Fill Data of Materials Array
-            m_arrMat = new Dictionary<EMemberType_FS_Position, CMat>(); // CMat[1];
-            m_arrMat[0] = m_arrMembers[0].CrScStart.m_Mat;
 
-            // Cross-sections
-            // CrSc List - CrSc Array - Fill Data of Cross-sections Array
+            // Materials            
+            m_arrMat = new Dictionary<EMemberType_FS_Position, CMat>();
             // Cross-section
-            m_arrCrSc = new Dictionary<EMemberType_FS_Position, CCrSc>(); //new CCrSc[2];
-            m_arrCrSc[0] = m_arrMembers[0].CrScStart;
-            m_arrCrSc[0].m_Mat = m_arrMembers[0].CrScStart.m_Mat; // Set CrSc Material
+            m_arrCrSc = new Dictionary<EMemberType_FS_Position, CCrSc>();
 
-            m_arrCrSc.Add((EMemberType_FS_Position)1, m_arrMembers[1].CrScStart);
-            m_arrCrSc[(EMemberType_FS_Position)1].m_Mat = m_arrMembers[1].CrScStart.m_Mat;
+            //initialize Materials and Cross Sections according to members types
+            foreach (CMember m in m_arrMembers)
+            {
+                if (!m_arrMat.ContainsKey(m.EMemberTypePosition))
+                {
+                    m_arrMat[m.EMemberTypePosition] = m.CrScStart.m_Mat;
+                }
+                if (!m_arrCrSc.ContainsKey(m.EMemberTypePosition))
+                {
+                    m_arrCrSc[m.EMemberTypePosition] = m.CrScStart;
+                    m_arrCrSc[m.EMemberTypePosition].m_Mat = m.CrScStart.m_Mat;  //to Mato - treba aj toto? nejako mi to pride,ze je to dvojmo
+                }
+            }
 
-            //m_arrCrSc[1] = m_arrMembers[1].CrScStart;
-            //m_arrCrSc[1].m_Mat = m_arrMembers[1].CrScStart.m_Mat; // Set CrSc Material
+            //// Materials
+            //// Materials List - Materials Array - Fill Data of Materials Array
+            //m_arrMat = new Dictionary<EMemberType_FS_Position, CMat>(); // CMat[1];
+            //m_arrMat[0] = m_arrMembers[0].CrScStart.m_Mat;
 
-            // TODO 680 - pridat materialy a prierezy pre ER a MR canopy
+            //// Cross-sections
+            //// CrSc List - CrSc Array - Fill Data of Cross-sections Array
+            //// Cross-section
+            //m_arrCrSc = new Dictionary<EMemberType_FS_Position, CCrSc>(); //new CCrSc[2];
+            //m_arrCrSc[0] = m_arrMembers[0].CrScStart;
+            //m_arrCrSc[0].m_Mat = m_arrMembers[0].CrScStart.m_Mat; // Set CrSc Material
+
+            //m_arrCrSc.Add((EMemberType_FS_Position)1, m_arrMembers[1].CrScStart);
+            //m_arrCrSc[(EMemberType_FS_Position)1].m_Mat = m_arrMembers[1].CrScStart.m_Mat;
+
+            ////m_arrCrSc[1] = m_arrMembers[1].CrScStart;
+            ////m_arrCrSc[1].m_Mat = m_arrMembers[1].CrScStart.m_Mat; // Set CrSc Material
+
+            //// TODO 680 - pridat materialy a prierezy pre ER a MR canopy
         }
     }
 }
