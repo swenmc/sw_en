04f5541b39be48d2ccae16e4a394a8e695c29ce5
sw_en <sw.en.mc@gmail.com>
2021-01-29 15:34:37 +0100

    Komentar

--- PFD/ViewModels/CanopiesOptionsViewModel.cs
+++ PFD/ViewModels/CanopiesOptionsViewModel.cs
@@ -404,6 +404,9 @@ namespace PFD
 
             foreach (CCanopiesInfo ci in CanopiesList)
             {
+                // To Ondrej - toto sa mi velmi nepozdáva, pýtame sa či existuje viac než jedna purlin na ktorých by mohlo byt napojené
+                // PurlinBlockCanopy, alebo chceme vedieť koľko tých PurlinBlockCanopy bude
+                // to totiž závisí na hodnote ILS, ktorá je nastavená pre PurlinCanopy
                 if (ci.PurlinCountLeft > 1 || ci.PurlinCountRight > 1) return true;
             }
 
