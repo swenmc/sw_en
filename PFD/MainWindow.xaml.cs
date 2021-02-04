ff32b2438e9d4dea86b7922901757ae6bdfafe27
ondro <ondro@ONDRO-PC>
2021-02-04 10:21:47 +0100

    refaktoring

--- PFD/MainWindow.xaml.cs
+++ PFD/MainWindow.xaml.cs
@@ -1156,13 +1156,15 @@ namespace PFD
             ci = compList.ComponentList.FirstOrDefault(c => c.MemberTypePosition == EMemberType_FS_Position.EdgeRafterCanopy);
             if (ci != null) sGeometryInputData.iRafterCanopyFlyBracingEveryXXPurlin = ci.ILS_Items.IndexOf(ci.ILS);
 
-            ci = compList.ComponentList.FirstOrDefault(c => c.MemberTypePosition == EMemberType_FS_Position.MainRafterCanopy);
-            if (ci != null) sGeometryInputData.iRafterCanopyFlyBracingEveryXXPurlin = ci.ILS_Items.IndexOf(ci.ILS);
+            //ci = compList.ComponentList.FirstOrDefault(c => c.MemberTypePosition == EMemberType_FS_Position.MainRafterCanopy);
+            //if (ci != null) sGeometryInputData.iRafterCanopyFlyBracingEveryXXPurlin = ci.ILS_Items.IndexOf(ci.ILS);
 
             ci = compList.ComponentList.FirstOrDefault(c => c.MemberTypePosition == EMemberType_FS_Position.PurlinCanopy);
             if (ci != null) sGeometryInputData.iPurlinCanopy_ILS_Number = ci.ILS_Items.IndexOf(ci.ILS);
 
-            
+            //ci = compList.ComponentList.FirstOrDefault(c => c.MemberTypePosition == EMemberType_FS_Position.EdgePurlinCanopy);
+            //if (ci != null) sGeometryInputData.iPurlinCanopy_ILS_Number = ci.ILS_Items.IndexOf(ci.ILS);
+
         }
 
         private void SetUIElementsVisibility()
