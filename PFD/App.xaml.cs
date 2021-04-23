a462f967ec891d944e9f4b1e9b87320e079b1287
ondro <ondro@ONDRO-PC>
2021-04-24 01:15:09 +0200

    bug 792

--- PFD/App.xaml.cs
+++ PFD/App.xaml.cs
@@ -13,7 +13,13 @@ namespace PFD
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {        
-        
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var comException = e.Exception as System.Runtime.InteropServices.COMException;
+
+            if (comException != null && comException.ErrorCode == -2147221040)
+                e.Handled = true;
+        }
     }
 }
