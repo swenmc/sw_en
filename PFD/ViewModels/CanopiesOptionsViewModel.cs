e6e213ff9ce00b8506963974986cd49061dc6e3f
sw_en <sw.en.mc@gmail.com>
2021-06-03 15:28:45 +0200

    komentar

--- PFD/ViewModels/CanopiesOptionsViewModel.cs
+++ PFD/ViewModels/CanopiesOptionsViewModel.cs
@@ -512,11 +512,13 @@ namespace PFD
                     CCanopiesInfo previousCanopy = GetPreviousNeighboringCanopyLeft(canopy);
                     if (previousCanopy == null)
                         len += canopy.WidthLeft; // TO Ondrej, ako sirku canopy by sme mali uvazovat to co je v komentari (cely vyraz)
+                    // Horizontalny rozmer canopy este treba previest na sikmu dlzku takze cely vyraz Math.Cos(Math.Abs(Roof_Pitch_rad) 
 
                     CCanopiesInfo nextCanopy = GetNextNeighboringCanopyLeft(canopy);
                     if (nextCanopy == null)
                         // TO Ondrej, ako sirku canopy by sme mali uvazovat to co je v komentari (cely vyraz)
                         len += canopy.WidthLeft; // canopy.WidthLeft + canopyOverhangOffset_x - column_crsc_z_plus_temp - roofEdgeOverhang_X;
+                    // Horizontalny rozmer canopy este treba previest na sikmu dlzku takze cely vyraz Math.Cos(Math.Abs(Roof_Pitch_rad) 
 
                     else
                         len += Math.Abs(nextCanopy.WidthLeft - canopy.WidthLeft); // TO Ondrej, ako sirku canopy by sme mali uvazovat to co je v komentari (cely vyraz)
@@ -527,11 +529,13 @@ namespace PFD
                     CCanopiesInfo previousCanopy = GetPreviousNeighboringCanopyRight(canopy);
                     if (previousCanopy == null)
                         // TO Ondrej, ako sirku canopy by sme mali uvazovat to co je v komentari (cely vyraz)
+                        // Horizontalny rozmer canopy este treba previest na sikmu dlzku takze cely vyraz Math.Cos(Math.Abs(Roof_Pitch_rad) 
                         len += canopy.WidthRight; // canopyNext.WidthRight + canopyOverhangOffset_x - column_crsc_z_plus_temp - roofEdgeOverhang_X
 
                     CCanopiesInfo nextCanopy = GetNextNeighboringCanopyRight(canopy);
                     if (nextCanopy == null)
                         len += canopy.WidthRight; // TO Ondrej, ako sirku canopy by sme mali uvazovat to co je v komentari (cely vyraz)
+                    // Horizontalny rozmer canopy este treba previest na sikmu dlzku takze cely vyraz Math.Cos(Math.Abs(Roof_Pitch_rad) 
                     else
                         len += Math.Abs(nextCanopy.WidthRight - canopy.WidthRight); // TO Ondrej, ako sirku canopy by sme mali uvazovat to co je v komentari (cely vyraz)
                 }
