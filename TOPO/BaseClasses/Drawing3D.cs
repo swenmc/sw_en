c2ff79b8f93621db9958a524e6f122e9052dff99
777ondro <777ondro@gmail.com>
2021-06-29 12:27:46 +0200

    skuska 2

--- TOPO/BaseClasses/Drawing3D.cs
+++ TOPO/BaseClasses/Drawing3D.cs
@@ -5764,6 +5764,7 @@ namespace BaseClasses
             }
         }
 
+
         private static void AddMemberWireFramePoints(CMember m, GeometryModel3D gm3d)
         {
             MeshGeometry3D mesh = gm3d.Geometry as MeshGeometry3D;
