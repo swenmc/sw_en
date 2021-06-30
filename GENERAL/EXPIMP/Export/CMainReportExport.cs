7aa0463387123ecfaa6ac1cd09dbbdffe0f6d7a4
ondro <ondro@ONDRO-PC>
2021-06-30 14:44:22 +0200

    860

--- GENERAL/EXPIMP/Export/CMainReportExport.cs
+++ GENERAL/EXPIMP/Export/CMainReportExport.cs
@@ -66,7 +66,7 @@ namespace EXPIMP
 
             contents = new List<string[]>();
 
-            //XGraphics TitlePage_gfx = DrawTitlePage(s_document, projectInfo, modelData, exportOpts);
+            XGraphics TitlePage_gfx = DrawTitlePage(s_document, projectInfo, modelData, exportOpts);
 
             if(exportOpts.ExportModel3D)
                 DrawModel3D(s_document, modelData, exportOpts);
@@ -90,7 +90,7 @@ namespace EXPIMP
             if (exportOpts.ExportStandardDetails && modelData.HasCladding) 
                 DrawStandardDetails(s_document, modelData, exportOpts);
 
-            //AddTitlePageContentTableToDocument(TitlePage_gfx, contents);
+            AddTitlePageContentTableToDocument(TitlePage_gfx, contents);
 
             string fileName = GetReportPDFName();
             // Save the s_document...
@@ -495,7 +495,12 @@ namespace EXPIMP
 
             foreach (EViewCladdingFilters view in list_views)
             {
-                if(view == EViewCladdingFilters.CLADDING_ROOF) opts = GetModelViewsDisplayOptions(data, EDisplayOptionsTypes.Layouts_CW_Roof);
+                if (view == EViewCladdingFilters.CLADDING_ROOF)
+                {
+                    opts = GetModelViewsDisplayOptions(data, EDisplayOptionsTypes.Layouts_CW_Roof);
+                    opts.LY_ViewsPageSize = (EPageSizes)exportOpts.ExportPageSizeViewsCladding;
+                    opts.LY_ExportImagesQuality = (EImagesQuality)exportOpts.ExportImagesQuality;
+                }
 
                 sheetNo++;
                 Trace.WriteLine(sheetNo + ". " + view.ToString());
